f7dfbb25b3cb3e40af50812f000022f8b7c33cc3
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-10-21 16:53:02 +0200

    Add vite config

---
+++ packages/fdm-calculator/vite.config.ts
@@ -0,0 +1,47 @@
+// vite.config.ts
+
+/// <reference types="vitest" />
+// Configure Vitest (https://vitest.dev/config/)
+
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'fdm-calculator',
+      fileName: 'fdm-calculator',
+    },
+    rollupOptions: {
+        external: [
+          "node:util",
+          "node:buffer",
+          "node:stream",
+          "node:net",
+          "node:url",
+          "node:fs",
+          "node:path",
+          "perf_hooks",
+        ],
+        output: {
+          globals: {
+            "node:stream": "stream",
+            "node:buffer": "buffer",
+            "node:util": "util",
+            "node:net": "net",
+            "node:url": "url",
+            "node:fs": "fs",
+            "node:path": "path",
+            perf_hooks: "perf_hooks",
+          },
+          inlineDynamicImports: true,
+        },
+      }
+  },  
+  plugins: [dts()],
+  test: {
+    // ...
+  } 
+});
