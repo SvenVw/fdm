b08d78b6c56a5b4dda7ba90bf95f045d0e836fe3
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-10-31 17:19:32 +0100

    Add workaround for fdm-data test to connect to fdm

--- fdm-data/src/fertilizers/index.test.ts
+++ fdm-data/src/fertilizers/index.test.ts
@@ -1,13 +1,48 @@
 import { describe, expect, it, beforeEach } from 'vitest'
-import { createFdmLocal, migrateFdmLocal, type FdmType } from '@svenvw/fdm-core'
+import { fdmSchema as schema, type FdmType } from '@svenvw/fdm-core'
 import { extendFertilizersCatalogue } from '.'
 
-describe('Fertilizers Data', () => {
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+
+describe('Fertilizers Data [server]', () => {
     let fdm: FdmType
 
     beforeEach(async () => {
-        fdm = await createFdmLocal('memory://')
-        await migrateFdmLocal(fdm)
+
+        const host = process.env.POSTGRES_HOST
+        const port = Number(process.env.POSTGRES_PORT)
+        const user = process.env.POSTGRES_USER
+        const password = process.env.POSTGRES_PASSWORD
+        const database = process.env.POSTGRES_DB
+        const migrationsFolderPath = 'node_modules/@svenvw/fdm-core/dist/db/migrations'
+        
+        // Does not work yet :(
+        // const fdm = await createFdmServer(
+        //     host,
+        //     port,
+        //     user,
+        //     password,
+        //     database
+        //   )
+        // await migrateFdmServer(fdm)
+
+        // Workaround
+        fdm = drizzle({
+            connection : {
+              user : user,
+              password : password,
+              host : host,
+              port : port,
+              database : database
+            },
+            logger: false,
+            schema: schema
+          })
+          
+          // Run migration
+          await migrate(fdm, { migrationsFolder: migrationsFolderPath, migrationsSchema: 'fdm-migrations' })
+        
     })
 
     it('should throw error if dataset is not recognized', async () => {
