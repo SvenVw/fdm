3628a25a191d62b3b72e92a3625ebd854bc22626
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-11-26 16:13:12 +0100

    Improve documentation

--- fdm-core/src/cultivation.ts
+++ fdm-core/src/cultivation.ts
@@ -6,10 +6,10 @@ import { type FdmType } from './fdm'
 import { getCultivationType } from './cultivation.d'
 
 /**
- * Get cultivations available in catalogue
+ * Retrieves cultivations available in the catalogue.
  *
- * @param fdm - 
- * @returns A Promise that resolves with an array of cultivations and the details.
+ * @param fdm The FDM instance.
+ * @returns A Promise that resolves with an array of cultivation catalogue entries.
  * @alpha
  */
 export async function getCultivationsFromCatalogue(fdm: FdmType): Promise<schema.cultivationsCatalogueTypeSelect[]> {
@@ -22,10 +22,12 @@ export async function getCultivationsFromCatalogue(fdm: FdmType): Promise<schema
 }
 
 /**
- * Add cultivation to the catalogue
+ * Adds a new cultivation to the catalogue.
  *
- * @param fdm - 
- * @returns A Promise that resolves when the cultivation is added to the catalogue
+ * @param fdm The FDM instance.
+ * @param properties The properties of the cultivation to add.
+ * @returns A Promise that resolves when the cultivation is added.
+ * @throws If the insertion fails.
  * @alpha
  */
 export async function addCultivationToCatalogue(
@@ -48,13 +50,14 @@ export async function addCultivationToCatalogue(
 
 
 /**
- * Add cultivation to field
+ * Adds a cultivation to a field.
  *
- * @param fdm - 
- * @param b_lu_catalogue - Catalogue id of the cultivation
- * @param b_id - ID of the field
- * @param b_sowing_date - Date on which the cultivation is started
- * @returns A Promise that resolves with the id of the cultivation
+ * @param fdm The FDM instance.
+ * @param b_lu_catalogue The catalogue ID of the cultivation.
+ * @param b_id The ID of the field.
+ * @param b_sowing_date The sowing date of the cultivation.
+ * @returns A Promise that resolves with the ID of the new cultivation.
+ * @throws If the field does not exist or if the insertion fails.
  * @alpha
  */
 export async function addCultivation(
@@ -105,26 +108,26 @@ export async function addCultivation(
 }
 
 /**
- * Get the details of a cultivation
- * 
- * @param fdm 
- * @param b_lu- ID of requested cultivation
- * @returns A promise that resolves with properties of requested cultivation
+ * Retrieves the details of a specific cultivation.
+ *
+ * @param fdm The FDM instance.
+ * @param b_lu The ID of the cultivation.
+ * @returns A promise that resolves with the cultivation details, or undefined if not found.
  */
 export async function getCultivation(fdm: FdmType, b_lu: schema.cultivationsTypeSelect['b_lu']): Promise<getCultivationType> {
 
     // Get properties of the requested cultivation
     const cultivation = await fdm
         .select({
-           b_lu: schema.cultivations.b_lu,
-           b_lu_catalogue: schema.cultivationsCatalogue.b_lu_catalogue,
-           b_lu_source: schema.cultivationsCatalogue.b_lu_source,
-           b_lu_name: schema.cultivationsCatalogue.b_lu_name,
-           b_lu_name_en: schema.cultivationsCatalogue.b_lu_name_en,
-           b_lu_hcat3: schema.cultivationsCatalogue.b_lu_hcat3,
-           b_lu_hcat3_name: schema.cultivationsCatalogue.b_lu_hcat3_name,
-           b_sowing_date: schema.fieldSowing.b_sowing_date,
-           b_id: schema.fieldSowing.b_id
+            b_lu: schema.cultivations.b_lu,
+            b_lu_catalogue: schema.cultivationsCatalogue.b_lu_catalogue,
+            b_lu_source: schema.cultivationsCatalogue.b_lu_source,
+            b_lu_name: schema.cultivationsCatalogue.b_lu_name,
+            b_lu_name_en: schema.cultivationsCatalogue.b_lu_name_en,
+            b_lu_hcat3: schema.cultivationsCatalogue.b_lu_hcat3,
+            b_lu_hcat3_name: schema.cultivationsCatalogue.b_lu_hcat3_name,
+            b_sowing_date: schema.fieldSowing.b_sowing_date,
+            b_id: schema.fieldSowing.b_id
         })
         .from(schema.cultivations)
         .leftJoin(schema.fieldSowing, eq(schema.fieldSowing.b_lu, schema.cultivations.b_lu))
@@ -132,9 +135,21 @@ export async function getCultivation(fdm: FdmType, b_lu: schema.cultivationsType
         .where(eq(schema.cultivations.b_lu, b_lu))
         .limit(1)
 
+    // If no cultivation is found return an error
+    if (cultivation.length === 0) {
+        throw new Error('Cultivation does not exist')
+    }
+
     return cultivation[0]
 }
 
+/**
+ * Retrieves all cultivations for a given field.
+ *
+ * @param fdm The FDM instance.
+ * @param b_id The ID of the field.
+ * @returns A Promise that resolves with an array of cultivation details.
+ */
 export async function getCultivations(fdm: FdmType, b_id: schema.fieldSowingTypeSelect['b_id']): Promise<getCultivationType[]> {
 
     const cultivations = await fdm
@@ -158,11 +173,12 @@ export async function getCultivations(fdm: FdmType, b_id: schema.fieldSowingType
 }
 
 /**
- * Remove cultivation from farm
+ * Removes a cultivation from a field.
  *
- * @param fdm - 
- * @param b_lu - ID of the cultivation to be removed
- * @returns A Promise that resolves when the cultivation is removed from the field
+ * @param fdm The FDM instance.
+ * @param b_lu The ID of the cultivation to remove.
+ * @returns A Promise that resolves when the cultivation is removed.
+ * @throws If the deletion fails.
  * @alpha
  */
 export async function removeCultivation(
@@ -183,7 +199,7 @@ export async function removeCultivation(
         }
         catch (error) {
             tx.rollback()
-            throw new Error('removeCultivation failed with error ' + error)
+            throw new Error(`Failed to remove cultivation: ${error}`);
         }
     })
 }
