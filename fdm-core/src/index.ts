b1dea7704659d92395c62771b86087a71938dfc9
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-11-27 10:39:22 +0100

    Export type `FdmServerType`

--- fdm-core/src/index.ts
+++ fdm-core/src/index.ts
@@ -16,6 +16,7 @@
 import * as fdmSchema from './db/schema'
 export { fdmSchema }
 export {type FdmType } from './fdm.d'
+export {type FdmServerType } from './fdm-server.d'
 export { createFdmServer, migrateFdmServer } from './fdm-server'
 // export { createFdmLocal, migrateFdmLocal } from './fdm-local'
 export { addFarm, getFarm, updateFarm, } from './farm'
