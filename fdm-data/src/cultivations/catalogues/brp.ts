23c38a76ece0046cc08baa8aab0f9dc152dfb573
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-11-27 13:37:21 +0100

    Fix

--- fdm-data/src/cultivations/catalogues/brp.ts
+++ fdm-data/src/cultivations/catalogues/brp.ts
@@ -8,7 +8,6 @@ import { fdmSchema } from '@svenvw/fdm-core'
  */
 export function getCatalogueBrp(): fdmSchema.cultivationsCatalogueTypeInsert[] {
 
-    const catalogueSrm = brp.map(cultivation => {
     const catalogueBrp = brp.map(cultivation => {
         return {
             b_lu_source: 'brp',
