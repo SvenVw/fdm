3b359f603a02c969ab6fbb33da4c1cc2098e66e0
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-12-03 16:05:10 +0100

    Remove duplicate

--- fdm-app/app/routes/app.addfarm.$b_id_farm.cultivations.tsx
+++ fdm-app/app/routes/app.addfarm.$b_id_farm.cultivations.tsx
@@ -110,22 +110,7 @@ export default function Index() {
                         </aside>
                         <div className="flex-1 lg:max-w-2xl"><Outlet /></div>
                     </div>
-                </div>
-                <div className="hidden space-y-6 p-10 pb-16 md:block">
-                    <div className="space-y-0.5">
-                        <h2 className="text-2xl font-bold tracking-tight">Bouwplan</h2>
-                        <p className="text-muted-foreground">
-                            Werk de eigenschappen per gewas in je bouwplan bij.
-                        </p>
-                    </div>
-                    <Separator className="my-6" />
-                    <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-                        <aside className="-mx-4 lg:w-1/5">
-                            <SidebarNav items={loaderData.sidebarNavItems} />
-                        </aside>
-                        <div className="flex-1 lg:max-w-2xl"><Outlet /></div>
-                    </div>
-                </div>
+                </div>              
             </main>
             <Toaster />
         </SidebarInset >
