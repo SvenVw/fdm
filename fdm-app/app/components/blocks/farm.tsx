aa8ed087a2ebabc18c30ac24bbbfba38a813f2c6
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-12-09 16:41:11 +0100

    Add validation of form

--- fdm-app/app/components/blocks/farm.tsx
+++ fdm-app/app/components/blocks/farm.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useNavigation, Form } from "@remix-run/react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRemixForm, RemixFormProvider } from "remix-hook-form"
+import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -12,6 +15,14 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 
 import { MultiSelect } from "@/components/custom/multi-select"
 
@@ -30,69 +41,101 @@ export interface farmType {
     action: "/app/addfarm/new"
 }
 
+const FormSchema = z.object({
+    b_name_farm: z.string().min(3, {
+        message: "Naam van bedrijf moet minimaal 3 karakters bevatten",
+    }),
+})
+
+
 export function Farm(props: farmType) {
+    const navigation = useNavigation();
+
     const organicFertilizersList = props.organicFertilizersList
     const mineralFertilizersList = props.mineralFertilizersList
     const [selectedOrganicFertilizers, setOrganicFertilizers] = useState<string[]>(props.b_fertilizers_organic);
     const [selectedMineralFertilizers, setMineralFertilizers] = useState<string[]>(props.b_fertilizers_mineral);
 
-    const navigation = useNavigation();
+    const form = useRemixForm<z.infer<typeof FormSchema>>({
+        mode: "onTouched",
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            b_name_farm: "",
+        },
+    })
 
     return (
         <div className="flex h-screen items-center justify-center">
             <Card className="w-[350px]">
-                <Form method="post" action={props.action}>
-                    <fieldset
-                        disabled={navigation.state === "submitting"}
-                    >
-                        <CardHeader>
-                            <CardTitle>Bedrijf</CardTitle>
-                            <CardDescription>Wat voor soort bedrijf heb je?</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <div className="grid w-full items-center gap-4">
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="b_name_farm">Bedrijfsnaam <span className="text-red-500">*</span></Label>
-                                    <Input id="b_name_farm" name="b_name_farm" placeholder="Bv. Jansen V.O.F." aria-required="true"/>
+                <RemixFormProvider {...form}>
+                    <Form id="formFarm" onSubmit={form.handleSubmit}>
+                        {/* <fieldset
+                            disabled={navigation.state === "submitting"}
+                        > */}
+                            <CardHeader>
+                                <CardTitle>Bedrijf</CardTitle>
+                                <CardDescription>Wat voor soort bedrijf heb je?</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <FormField
+                                            control={form.control}
+                                            name="b_name_farm"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Bedrijfsnaam <span className="text-red-500">*</span></FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Bv. Jansen V.O.F." aria-required="true" {...field} />
+                                                    </FormControl>
+                                                    <FormDescription>
+                                                        De naam van je bedrijf
+                                                    </FormDescription>
+                                                    <FormMessage />
+                                                </FormItem>
+
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="b_fertilizers_organic">Welke <i>organische</i> meststoffen gebruikt u?</Label>
+                                        <MultiSelect
+                                            options={organicFertilizersList}
+                                            onValueChange={setOrganicFertilizers}
+                                            defaultValue={selectedOrganicFertilizers}
+                                            placeholder="Selecteer meststoffen"
+                                            variant="inverted"
+                                            animation={0}
+                                            maxCount={10}
+                                            name="b_fertilizers_organic"
+                                        />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="b_fertilizers_mineral">Welke <i>minerale</i> meststoffen gebruikt u?</Label>
+                                        <MultiSelect
+                                            options={mineralFertilizersList}
+                                            onValueChange={setMineralFertilizers}
+                                            defaultValue={selectedMineralFertilizers}
+                                            placeholder="Selecteer meststoffen"
+                                            variant="inverted"
+                                            animation={0}
+                                            maxCount={10}
+                                            name="b_fertilizers_mineral"
+                                        />
+                                    </div>
                                 </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="b_fertilizers_organic">Welke <i>organische</i> meststoffen gebruikt u?</Label>
-                                    <MultiSelect
-                                        options={organicFertilizersList}
-                                        onValueChange={setOrganicFertilizers}
-                                        defaultValue={selectedOrganicFertilizers}
-                                        placeholder="Selecteer meststoffen"
-                                        variant="inverted"
-                                        animation={0}
-                                        maxCount={10}
-                                        name="b_fertilizers_organic"
-                                    />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="b_fertilizers_mineral">Welke <i>minerale</i> meststoffen gebruikt u?</Label>
-                                    <MultiSelect
-                                        options={mineralFertilizersList}
-                                        onValueChange={setMineralFertilizers}
-                                        defaultValue={selectedMineralFertilizers}
-                                        placeholder="Selecteer meststoffen"
-                                        variant="inverted"
-                                        animation={0}
-                                        maxCount={10}
-                                        name="b_fertilizers_mineral"
-                                    />
-                                </div>
-                            </div>
-                        </CardContent>
-                        <CardFooter className="flex justify-between">
-                            <Button variant="outline">Terug</Button>
-                            <Button type="submit">
-                                {navigation.state === "submitting"
-                                    ? "Opslaan..."
-                                    : "Verder"}
-                            </Button>
-                        </CardFooter>
-                    </fieldset>
-                </Form>
+                            </CardContent>
+                            <CardFooter className="flex justify-between">
+                                <Button variant="outline">Terug</Button>
+                                <Button type="submit">
+                                    {navigation.state === "submitting"
+                                        ? "Opslaan..."
+                                        : "Verder"}
+                                </Button>
+                            </CardFooter>
+                        {/* </fieldset> */}
+                    </Form>
+                </RemixFormProvider>
             </Card>
         </div>
 
