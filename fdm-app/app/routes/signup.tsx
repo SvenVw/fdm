293bfe42fa42f6653c49825cb1f38aaf78cf5be0
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-12-10 13:30:37 +0100

    Remove not needed component

--- fdm-app/app/routes/signup.tsx
+++ fdm-app/app/routes/signup.tsx
@@ -13,7 +13,6 @@ import { signUpUser, getUserFromSession } from "@svenvw/fdm-core"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   FormControl,
   FormDescription,
