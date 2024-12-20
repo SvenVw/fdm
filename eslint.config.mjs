4b6606e807c103878fb778ba0fbb4c68bc5e54e8
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-12-20 13:22:55 +0100

    Switch off the camelcase rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,7 @@ export default neostandard({
   ts: true,
   files: ['{fdm-core,fdm-data,fdm-calculator}/src/**/*.{js,mjs,cjs,ts}', 'fdm-app/app/**/*.{js,mjs,cjs,ts}'],
   ignores: resolveIgnoresFromGitignore(),
+  rules: {
+    camelcase: 'off'
+  }
 })
