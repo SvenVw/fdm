b6f22081c47d08e89adbab8f2f52dbb23db7daf4
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-12-20 10:57:29 +0100

    Improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
-import neostandard from 'neostandard'
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
 
 export default neostandard({
-  // options
+  ts: true,
+  files: ['{fdm-core,fdm-data,fdm-calculator}/src/**/*.{js,mjs,cjs,ts}', 'fdm-app/app/**/*.{js,mjs,cjs,ts}'],
+  ignores: resolveIgnoresFromGitignore(),
 })
