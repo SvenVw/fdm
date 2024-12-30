5909885c2433d1be561bc7cf77953c311541b8a7
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2024-12-30 15:58:28 +0100

    Setup viewstate

--- fdm-app/app/components/custom/map-fields.tsx
+++ fdm-app/app/components/custom/map-fields.tsx
@@ -1,35 +1,60 @@
 import { Map as MapGL, GeolocateControl, NavigationControl, Source, Layer } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import geojsonExtent from '@mapbox/geojson-extent'
+import type { FeatureCollection } from "geojson";
+import { useMemo, useState } from 'react';
 
 export function MapFields({
     interactive,
     mapboxToken,
-    mapStyle
- }: MapFieldsProps) {
+    mapStyle,
+    fieldsSelected,
+}: MapFieldsProps) {
 
 
     // Set controls
     let Controls = <></>
     if (interactive === true) {
-        Controls = <div><GeolocateControl/><NavigationControl/></div>
+        Controls = <div><GeolocateControl /><NavigationControl /></div>
+    }
+
+    // Set viewState
+    const initialBounds = [3.1, 50.7, 7.2, 53.6];
+    const [viewState, setViewState] = useState({
+        bounds: initialBounds,
+        fitBoundsOptions: {
+            padding: 0
+        }
+    });
+    if (fieldsSelected) {
+        const bounds = useMemo(() => {
+            try {
+                return geojsonExtent(fieldsSelected);
+            } catch (error) {
+                console.error('Failed to calculate bounds:', error);
+                return initialBounds; 
+            }
+        }, [fieldsSelected]);
+        setViewState({
+            bounds: bounds,
+            fitBoundsOptions: {
+                padding: 10
+            }
+        })
     }
 
     return (
         <MapGL
-            initialViewState={{
-                longitude: 5,
-                latitude: 52,
-                zoom: 10,
-            }}
+            {...viewState}
             style={{ height: "calc(100vh - 64px - 123px)" }}
             interactive={interactive}
             mapStyle={mapStyle}
             mapboxAccessToken={mapboxToken}
-            // onZoomEnd={async evt => await loadBrpFields(evt)}
-            // onMoveEnd={async evt => await loadBrpFields(evt)}
-            // onClick={evt => handleClickOnField(evt)}
-            // interactiveLayerIds={['brp-fields-fill', 'selected-fields-fill', 'brp-fields-line']}
-        >       
+        // onZoomEnd={async evt => await loadBrpFields(evt)}
+        // onMoveEnd={async evt => await loadBrpFields(evt)}
+        // onClick={evt => handleClickOnField(evt)}
+        // interactiveLayerIds={['brp-fields-fill', 'selected-fields-fill', 'brp-fields-line']}
+        >
 
             {Controls}
         </MapGL>
@@ -40,6 +65,7 @@ interface MapFieldsProps {
     interactive: boolean
     mapboxToken: string
     mapStyle: "mapbox://styles/mapbox/satellite-streets-v12"
+    fieldsSelected: FeatureCollection | null
 }
 
 interface GeoJSONFeature {
