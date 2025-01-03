981725c4f40fe119343392e0623da923e5f07ff1
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-03 13:58:45 +0100

    Change color

--- fdm-app/app/components/custom/atlas-fields-panel.tsx
+++ fdm-app/app/components/custom/atlas-fields-panel.tsx
@@ -76,7 +76,7 @@ export default function FieldsPanel({ fields }: { fields: FeatureCollection }) {
                                             key={index}
                                             className="mb-2 grid grid-cols-[25px_1fr] items-start pb-2 last:mb-0 last:pb-0"
                                         >
-                                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-green-500" />
                                             <div className="space-y-1">
                                                 <p className="text-sm font-medium leading-none">
                                                     {cultivation.b_lu_name}
