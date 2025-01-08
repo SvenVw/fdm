7676cac9489ac0e46081ca1bacf9c40d320c6a3d
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-08 10:27:24 +0100

    Setup settings page of farm

--- fdm-app/app/routes/farm.$b_id_farm.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.tsx
@@ -10,6 +10,7 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
+import { SidebarPage } from "@/components/custom/sidebar-page";
 
 // Blocks
 
@@ -17,7 +18,6 @@ export async function loader({
     request, params
 }: LoaderFunctionArgs) {
 
-    
     // Get the farm id
     const b_id_farm = params.b_id_farm
     if (!b_id_farm) {
@@ -26,15 +26,15 @@ export async function loader({
 
     // Get details of farm
     const farm = await getFarm(fdm, b_id_farm)
-    if (! farm) {
+    if (!farm) {
         throw data("Farm is not found", { status: 404, statusText: "Farm is not found" });
     }
 
     // Set farm to active
     await auth.api.updateUser({
-        body: {           
+        body: {
             farm_active: b_id_farm,
-        },        
+        },
         headers: request.headers
     })
 
@@ -47,10 +47,27 @@ export async function loader({
         }
     })
 
+    // Create the items for sidebar page
+    const sidebarPageItems = [
+        {
+            to: `/farm/${farm.b_id_farm}/properties`,
+            title: "Gegevens"
+        },
+        {
+            to: `/farm/${farm.b_id_farm}/access`,
+            title: "Toegang"
+        },
+        {
+            to: `/farm/${farm.b_id_farm}/delete`,
+            title: "Verwijderen"
+        }
+    ]
+
     // Return user information from loader
     return {
         farm: farm,
-        farmOptions: farmOptions
+        farmOptions: farmOptions,
+        sidebarPageItems: sidebarPageItems
     }
 }
 
@@ -58,8 +75,7 @@ export default function FarmPage() {
     const loaderData = useLoaderData<typeof loader>()
 
     return (
-
-         <SidebarInset>
+        <SidebarInset>
             <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
@@ -98,7 +114,23 @@ export default function FarmPage() {
                 </Breadcrumb>
             </header>
             <main>
-                <p>{loaderData.farm.b_name_farm}</p>
+                <div className="space-y-6 p-10 pb-16">
+                    <div className="flex items-center">
+                        <div className="space-y-0.5">
+                            <h2 className="text-2xl font-bold tracking-tight">Bedrijf</h2>
+                            <p className="text-muted-foreground">
+                                Beheer de gegevens en instellingen van dit bedrijf
+                            </p>
+                        </div>
+                    </div>
+                    <Separator className="my-6" />
+                    <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+                        <aside className="-mx-4 lg:w-1/5">
+                            <SidebarPage items={loaderData.sidebarPageItems} />
+                        </aside>
+                        <div className="flex-1 lg:max-w-2xl"><Outlet /></div>
+                    </div>
+                </div>
             </main>
         </SidebarInset >
     )
