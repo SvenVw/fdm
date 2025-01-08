91c531af1bb878556ae61006b2d2e44d3533820f
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-08 13:30:53 +0100

    Add access and delete page for farm

---
+++ fdm-app/app/routes/farm.$b_id_farm.delete.tsx
@@ -0,0 +1,118 @@
+import { ActionFunctionArgs, data, Form, LoaderFunctionArgs, useLoaderData } from "react-router";
+import { getFarm, updateFarm } from "@svenvw/fdm-core";
+import { fdm } from "../lib/fdm.server";
+import validator from 'validator';
+const { isPostalCode } = validator
+
+// Components
+import { Separator } from "@/components/ui/separator";
+import { RemixFormProvider, useRemixForm } from "remix-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { LoadingSpinner } from "@/components/custom/loadingspinner";
+import { dataWithError, dataWithSuccess } from "remix-toast";
+import { extractFormValuesFromRequest } from "@/lib/form";
+
+// Blocks
+
+export async function loader({
+    request, params
+}: LoaderFunctionArgs) {
+
+    // Get the farm id
+    const b_id_farm = params.b_id_farm
+    if (!b_id_farm) {
+        throw data("Farm ID is required", { status: 400, statusText: "Farm ID is required" });
+    }
+
+    // Get details of farm
+    const farm = await getFarm(fdm, b_id_farm)
+    if (!farm) {
+        throw data("Farm is not found", { status: 404, statusText: "Farm is not found" });
+    }
+
+    // Return user information from loader
+    return {
+        farm: farm,
+    }
+}
+
+export default function FarmAccessBlock() {
+    const loaderData = useLoaderData<typeof loader>()
+
+    const form = useRemixForm<z.infer<typeof FormSchema>>({
+        mode: "onTouched",
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+           
+        },
+    })
+
+    return (
+        <div className="space-y-6">
+            <div>
+                <h3 className="text-lg font-medium">Verwijderen</h3>
+                <p className="text-sm text-muted-foreground">
+                    Helaas, je hebt geen rechten om dit bedrijf te verwijderen
+                </p>
+            </div>
+            <Separator />
+            {/* <div className="flex flex-col space-y-1"> */}
+            <RemixFormProvider {...form}>
+                <Form id="formFarmDelete" onSubmit={form.handleSubmit} method="POST">
+                    <fieldset disabled={form.formState.isSubmitting}>
+                        <div className="grid grid-cols-2 w-full items-center gap-4">
+                           
+                        </div >
+                    </fieldset>
+                    <br />
+                    <div className="ml-auto">
+                        {/* <Button
+                            type="submit"
+                            disabled={form.formState.isSubmitting}
+                            className="m-auto"
+                        >
+                            {form.formState.isSubmitting && <LoadingSpinner />}
+                            Bijwerken
+                        </Button> */}
+                    </div>
+
+                </Form>
+            </RemixFormProvider>
+        </div >
+
+    )
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
+    const b_id_farm = params.b_id_farm;
+
+
+    if (!b_id_farm) {
+        return dataWithError(null, "Missing farm ID.");
+    }
+
+    try {
+        const formValues = await extractFormValuesFromRequest(request, FormSchema);
+
+        return dataWithSuccess(
+            `access is updated`,
+            { message: `${formValues.b_name_farm} verwijderd bijgewerkt` }
+        );
+    } catch (error) {
+        console.error("Failed to update farm:", error);
+        return dataWithError(
+            null,
+            `Er is iets misgegaan bij het verwijderen: ${error instanceof Error ? error.message : 'Onbekende fout'}`
+        );
+    }
+}
+
+
+// Form Schema
+const FormSchema = z.object({
+})
