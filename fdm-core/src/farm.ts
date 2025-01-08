ffa6bad7c147010ba883138d97871e1198f3f16e
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-08 12:16:24 +0100

    Update the functions for farm with the changed columns

--- fdm-core/src/farm.ts
+++ fdm-core/src/farm.ts
@@ -8,19 +8,23 @@ import { type FdmType } from './fdm'
 * Add a new farm.
 *
 * @param b_name_farm - Name of the farm
-* @param b_sector - Sector(s) for which the farm is active
+* @param b_businessid_farm - Business ID of the farm
+* @param b_address_farm - Address of the farm
+* @param b_postalcode_farm - Postal code of the farm
 * @returns A Promise that resolves when the farm has been added and returns the value for b_id_farm
 * @alpha
 */
-export async function addFarm(fdm: FdmType, b_name_farm: schema.farmsTypeInsert['b_name_farm'], b_sector: schema.farmsTypeInsert['b_sector']): Promise<schema.farmsTypeInsert['b_id_farm']> {
+export async function addFarm(fdm: FdmType, b_name_farm: schema.farmsTypeInsert['b_name_farm'], b_businessid_farm: schema.farmsTypeInsert['b_businessid_farm'], b_address_farm: schema.farmsTypeInsert['b_address_farm'], b_postalcode_farm: schema.farmsTypeInsert['b_postalcode_farm']): Promise<schema.farmsTypeInsert['b_id_farm']> {
     // Generate an ID for the farm
     const b_id_farm = createId()
 
-    // Insert the farm in the dab
+    // Insert the farm in the db
     const farmData = {
         b_id_farm,
         b_name_farm,
-        b_sector
+        b_businessid_farm,
+        b_address_farm,
+        b_postalcode_farm
     }
     await fdm
         .insert(schema.farms)
@@ -66,23 +70,29 @@ export async function getFarms(fdm: FdmType): Promise<schema.farmsTypeSelect[]> 
 *
 * @param b_id_farm - The id of the farm to be updated.
 * @param b_name_farm - The new value for the name of the farm.
-* @param b_sector - The new list of sectors for which this farm is active.
+* @param b_businessid_farm - The new value for the business ID of the farm.
+* @param b_address_farm - The new value for the address of the farm.
+* @param b_postalcode_farm - The new value for the postal code of the farm.
 * @returns A Promise that resolves with an object that contains the details of a farm.
 * @alpha
 */
-export async function updateFarm(fdm: FdmType, b_id_farm: schema.farmsTypeInsert['b_id_farm'], b_name_farm: schema.farmsTypeInsert['b_name_farm'], b_sector: schema.farmsTypeInsert['b_sector']): Promise<schema.farmsTypeSelect> {
+export async function updateFarm(fdm: FdmType, b_id_farm: schema.farmsTypeInsert['b_id_farm'], b_name_farm: schema.farmsTypeInsert['b_name_farm'], b_businessid_farm: schema.farmsTypeInsert['b_businessid_farm'], b_address_farm: schema.farmsTypeInsert['b_address_farm'], b_postalcode_farm: schema.farmsTypeInsert['b_postalcode_farm']): Promise<schema.farmsTypeSelect> {
     const updatedFarm = await fdm
         .update(schema.farms)
         .set({
             b_name_farm,
-            b_sector,
+            b_businessid_farm,
+            b_address_farm,
+            b_postalcode_farm,
             updated: new Date()
         })
         .where(eq(schema.farms.b_id_farm, b_id_farm))
         .returning({
             b_id_farm: schema.farms.b_id_farm,
             b_name_farm: schema.farms.b_name_farm,
-            b_sector: schema.farms.b_sector,
+            b_businessid_farm: schema.farms.b_businessid_farm,
+            b_address_farm: schema.farms.b_address_farm,
+            b_postalcode_farm: schema.farms.b_postalcode_farm,
             created: schema.farms.created,
             updated: schema.farms.updated
         })
