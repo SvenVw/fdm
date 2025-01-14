72ce439bfa15b327319241b2a05eeece9cde3890
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-14 12:59:11 +0100

    Add farm settings page

--- fdm-app/app/routes/farm.$b_id_farm.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.tsx
@@ -1,5 +1,21 @@
-import { redirect } from "react-router";
+import { auth } from "@/lib/auth.server";
+import { data, LoaderFunctionArgs } from "react-router";
 
-export function loader() {
-    return redirect('./settings')
+export async function loader({
+    request, params
+}: LoaderFunctionArgs) {
+
+    // Get the farm id
+    const b_id_farm = params.b_id_farm
+    if (!b_id_farm) {
+        throw data("Farm ID is required", { status: 400, statusText: "Farm ID is required" });
+    }
+
+    // Set the farm id to active
+    await auth.api.updateUser({
+        body: {
+            farm_active: b_id_farm,
+        },
+        headers: request.headers
+    })
 }
