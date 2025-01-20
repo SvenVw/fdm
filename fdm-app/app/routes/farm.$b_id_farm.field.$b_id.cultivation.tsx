336d52fb8f5fd819842f048a424f908f149fde12
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-20 15:36:08 +0100

    Add cultivations block for field

--- fdm-app/app/routes/farm.$b_id_farm.field.$b_id.cultivation.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.field.$b_id.cultivation.tsx
@@ -1,16 +1,34 @@
-import { FarmPagination } from "@/components/custom/farm/farm-pagination"
+import { CultivationsForm, FormSchema } from "@/components/custom/cultivations"
 import { Separator } from "@/components/ui/separator"
 import { fdm } from "@/lib/fdm.server"
-import { getCultivations, getField} from "@svenvw/fdm-core"
+import { extractFormValuesFromRequest } from "@/lib/form"
 import {
+    addCultivation,
+    getCultivations,
+    getCultivationsFromCatalogue,
+    getField,
+    removeCultivation,
+} from "@svenvw/fdm-core"
+
+import {
+    type ActionFunctionArgs,
     type LoaderFunctionArgs,
-    Outlet,
     data,
     useLoaderData,
     useLocation,
 } from "react-router"
+import { dataWithError, dataWithSuccess } from "remix-toast"
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
+    // Get the farm id
+    const b_id_farm = params.b_id_farm
+    if (!b_id_farm) {
+        throw data("Farm ID is required", {
+            status: 400,
+            statusText: "Farm ID is required",
+        })
+    }
+
     // Get the field id
     const b_id = params.b_id
     if (!b_id) {
@@ -29,31 +47,29 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
         })
     }
 
-    // Get cultivations of field
+    // Get available cultivations for the farm
+    const cultivationsCatalogue = await getCultivationsFromCatalogue(
+        fdm,
+        b_id_farm,
+    )
+    // Map cultivations to options for the combobox
+    const cultivationsCatalogueOptions = cultivationsCatalogue.map(
+        (cultivation) => {
+            return {
+                value: cultivation.b_lu_catalogue,
+                label: cultivation.b_lu_name,
+            }
+        },
+    )
+
+    // Get cultivations for the field
     const cultivations = await getCultivations(fdm, b_id)
-    if (!cultivations) {
-        throw data("Cultivations are not found", {
-            status: 404,
-            statusText: "Cultivations are not found",
-        })
-    }
-
-    const cultivationItems = cultivations.map((cultivation) => { 
-        return {
-            to: cultivation.b_lu,
-            label: cultivation.b_lu_name
-        }
-    })
-
-    cultivationItems.push({
-        to: "./create",
-        label: "Nieuw gewas"
-    })
 
     // Return user information from loader
     return {
         field: field,
-        cultivationItems: cultivationItems,
+        cultivationsCatalogueOptions: cultivationsCatalogueOptions,
+        cultivations: cultivations,
     }
 }
 
@@ -64,19 +80,68 @@ export default function FarmFieldsOverviewBlock() {
     return (
         <div className="space-y-6">
             <div>
-                <h3 className="text-lg font-medium">Gewasssen</h3>
+                <h3 className="text-lg font-medium">Gewassen</h3>
                 <p className="text-sm text-muted-foreground">
-                    Werk de gegevens bij van dit gewas
+                    Vul de gewassen in voor dit perceel.
                 </p>
             </div>
             <Separator />
-            <FarmPagination
-                items={loaderData.cultivationItems}
-                currentPath={location.pathname}
-            >
-                <Outlet/>
-            </FarmPagination>
+            <CultivationsForm
+                cultivations={loaderData.cultivations}
+                action={location.pathname}
+                options={loaderData.cultivationsCatalogueOptions}
+            />
         </div>
     )
 }
 
+export async function action({ request, params }: ActionFunctionArgs) {
+    // Get the field ID
+    const b_id = params.b_id
+    if (!b_id) {
+        return dataWithError(null, "Missing field ID.")
+    }
+
+    if (request.method === "POST") {
+        // Collect form entry
+        const formValues = await extractFormValuesFromRequest(
+            request,
+            FormSchema,
+        )
+        const { b_lu_catalogue, b_id, b_sowing_date } = formValues
+
+        await addCultivation(b_lu_catalogue, b_id, b_sowing_date)
+
+        return dataWithSuccess(
+            { result: "Data saved successfully" },
+            { message: "Gewas is toegevoegd! 🎉" },
+        )
+    }
+
+    if (request.method === "DELETE") {
+        const formData = await request.formData()
+        const b_lu = formData.get("b_lu")
+
+        if (!b_lu || typeof b_lu !== "string") {
+            return dataWithError(
+                "Invalid or missing b_lu value",
+                "Oops! Something went wrong. Please try again later.",
+            )
+        }
+
+        try {
+            await removeCultivation(fdm, b_lu)
+
+            return dataWithSuccess("Date deleted successfully", {
+                message: "Gewas is verwijderd",
+            })
+        } catch (error) {
+            // Handle errors appropriately. Log the error for debugging purposes.
+            console.error("Error deleting cultivation:", error)
+            return dataWithError(
+                error instanceof Error ? error.message : "Unknown error",
+                "Er is een fout opgetreden bij het verwijderen van het gewas. Probeer het later opnieuw.",
+            )
+        }
+    }
+}
