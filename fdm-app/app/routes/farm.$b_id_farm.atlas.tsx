7d3e299ac10ed4745239a087ef2a7ef90efca91f
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-27 16:51:14 +0100

    Add map page

---
+++ fdm-app/app/routes/farm.$b_id_farm.atlas.tsx
@@ -0,0 +1,129 @@
+import { FarmContent } from "@/components/custom/farm/farm-content"
+import { FarmHeader } from "@/components/custom/farm/farm-header"
+import { FarmTitle } from "@/components/custom/farm/farm-title"
+import { SidebarInset } from "@/components/ui/sidebar"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Toaster } from "@/components/ui/sonner"
+import { auth } from "@/lib/auth.server"
+import { fdm } from "@/lib/fdm.server"
+import { getFarm, getFarms } from "@svenvw/fdm-core"
+import {
+    type LoaderFunctionArgs,
+    Outlet,
+    data,
+    redirect,
+    useLoaderData,
+} from "react-router"
+import { ClientOnly } from "remix-utils/client-only"
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+    // Get the farm id
+    const b_id_farm = params.b_id_farm
+    if (!b_id_farm) {
+        throw data("Farm ID is required", {
+            status: 400,
+            statusText: "Farm ID is required",
+        })
+    }
+
+    // Get details of farm
+    let farm
+    try {
+        farm = await getFarm(fdm, b_id_farm)
+        if (!farm) {
+            throw data("Farm is not found", {
+                status: 404,
+                statusText: "Farm is not found",
+            })
+        }
+    } catch (error) {
+        console.error("Failed to fetch farm details:", error)
+        throw data("Failed to fetch farm details", {
+            status: 500,
+            statusText: "Internal Server Error",
+        })
+    }
+
+    // Set farm to active
+    try {
+        await auth.api.updateUser({
+            body: {
+                farm_active: b_id_farm,
+            },
+            headers: request.headers,
+        })
+    } catch (error) {
+        console.error("Failed to update active farm:", error)
+        throw data("Failed to update active farm", {
+            status: 500,
+            statusText: "Internal Server Error",
+        })
+    }
+
+    // Get a list of possible farms of the user
+    let farms: Awaited<ReturnType<typeof getFarms>>
+    try {
+        farms = await getFarms(fdm)
+    } catch (error) {
+        console.error("Failed to fetch farms list:", error)
+        throw data("Failed to fetch farms list", {
+            status: 500,
+            statusText: "Internal Server Error",
+        })
+    }
+
+    // Redirect to farms overview if user has no farm
+    if (farms.length === 0) {
+        return redirect("./farm")
+    }
+
+    const farmOptions = farms.map((farm) => {
+        return {
+            b_id_farm: farm.b_id_farm,
+            b_name_farm: farm.b_name_farm,
+        }
+    })
+
+    const layerOptions = [{ layerKey: "fields", layerName: "Percelen" }, {layerKey: "soil", layerName: "Bodem"}]
+
+    // Return user information from loader
+    return {
+        farm: farm,
+        b_id_farm: b_id_farm,
+        farmOptions: farmOptions,
+        layerOptions: layerOptions,
+        layerSelected: "fields",
+    }
+}
+
+export default function FarmContentBlock() {
+    const loaderData = useLoaderData<typeof loader>()
+
+    return (
+        <SidebarInset>
+            <FarmHeader
+                farmOptions={loaderData.farmOptions}
+                b_id_farm={loaderData.b_id_farm}
+                layerOptions={loaderData.layerOptions}
+                layerSelected={loaderData.layerSelected}
+                action={{
+                    to: `/farm/${loaderData.b_id_farm}/field`,
+                    label: "Naar percelen",
+                }}
+            />
+            <main>
+                <FarmTitle
+                    title={"Kaarten"}
+                    description={"Bekijk verschillende kaartlagen van je bedrijf"}
+                />
+                <ClientOnly
+                    fallback={<Skeleton className="h-full w-full rounded-xl" />}
+                >
+                    {() => (
+                        <Outlet />                     
+                    )}
+                </ClientOnly>              
+            </main>
+        </SidebarInset>
+    )
+}
