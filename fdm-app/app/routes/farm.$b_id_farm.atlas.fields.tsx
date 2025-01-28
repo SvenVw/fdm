990e71ca5ec8dcbc3466937d4993892fb28cf761
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-28 13:33:57 +0100

    Show fields on map

--- fdm-app/app/routes/farm.$b_id_farm.atlas.fields.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.atlas.fields.tsx
@@ -1,21 +1,9 @@
 import { AtlasFields } from "@/components/custom/atlas-fields"
-import { FarmContent } from "@/components/custom/farm/farm-content"
-import { FarmHeader } from "@/components/custom/farm/farm-header"
-import { FarmTitle } from "@/components/custom/farm/farm-title"
-import { SidebarInset } from "@/components/ui/sidebar"
-import { Skeleton } from "@/components/ui/skeleton"
-import { Toaster } from "@/components/ui/sonner"
 import { auth } from "@/lib/auth.server"
 import { fdm } from "@/lib/fdm.server"
-import { getFarm, getFarms } from "@svenvw/fdm-core"
-import {
-    type LoaderFunctionArgs,
-    Outlet,
-    data,
-    redirect,
-    useLoaderData,
-} from "react-router"
-import { ClientOnly } from "remix-utils/client-only"
+import { getFarm, getFarms, getFields } from "@svenvw/fdm-core"
+import { type LoaderFunctionArgs, data, useLoaderData } from "react-router"
+import wkx from "wkx"
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
     // Get the farm id
@@ -27,50 +15,24 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
         })
     }
 
-    // Get details of farm
-    let farm
-    try {
-        farm = await getFarm(fdm, b_id_farm)
-        if (!farm) {
-            throw data("Farm is not found", {
-                status: 404,
-                statusText: "Farm is not found",
-            })
-        }
-    } catch (error) {
-        console.error("Failed to fetch farm details:", error)
-        throw data("Failed to fetch farm details", {
-            status: 500,
-            statusText: "Internal Server Error",
-        })
-    }
-
-    // Set farm to active
-    try {
-        await auth.api.updateUser({
-            body: {
-                farm_active: b_id_farm,
+    // Get the fields of the farm
+    const fields = await getFields(fdm, b_id_farm)
+    // console.log(fields)
+    const features = fields.map((field) => {
+        const feature = {
+            type: "Feature",
+            properties: {
+                id: field.b_id,
+                name: field.b_name,
             },
-            headers: request.headers,
-        })
-    } catch (error) {
-        console.error("Failed to update active farm:", error)
-        throw data("Failed to update active farm", {
-            status: 500,
-            statusText: "Internal Server Error",
-        })
-    }
+            geometry: wkx.Geometry.parse(field.b_geometry).toGeoJSON(),
+        }
+        return feature
+    })
 
-    // Get a list of possible farms of the user
-    let farms: Awaited<ReturnType<typeof getFarms>>
-    try {
-        farms = await getFarms(fdm)
-    } catch (error) {
-        console.error("Failed to fetch farms list:", error)
-        throw data("Failed to fetch farms list", {
-            status: 500,
-            statusText: "Internal Server Error",
-        })
+    const featureCollection = {
+        type: "FeatureCollection",
+        features: features,
     }
 
     // Get the Mapbox token
@@ -78,6 +40,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
     // Return user information from loader
     return {
+        fieldsSelected: featureCollection,
         mapboxToken: mapboxToken,
     }
 }
@@ -89,10 +52,10 @@ export default function FarmContentBlock() {
         <AtlasFields
             height="calc(100vh - 64px - 123px)"
             width="100%"
-            interactive={false}
+            interactive={true}
             mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
             mapboxToken={loaderData.mapboxToken}
-            fieldsSelected={null}
+            fieldsSelected={loaderData.fieldsSelected}
             fieldsAvailableUrl={undefined}
         />
     )
