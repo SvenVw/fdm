590f62bcb6a1f2e85cad3132994fc2a0b752cfc1
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-01-29 11:55:55 +0100

    Add function to standardize error handling

---
+++ fdm-core/src/error.ts
@@ -0,0 +1,48 @@
+export function handleError(err: unknown, base: string, context?: Jsonable) {
+    const error = ensureError(err)
+
+    throw new BaseError(base, {
+        cause: error,
+        context: context,
+    })
+}
+
+function ensureError(value: unknown): Error {
+    if (value instanceof Error) return value
+
+    let stringified = "[Unable to stringify the thrown value]"
+    try {
+        stringified = JSON.stringify(value)
+    } catch {}
+
+    const error = new Error(
+        `This value was thrown as is, not through an Error: ${stringified}`,
+    )
+    return error
+}
+
+type Jsonable =
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | readonly Jsonable[]
+    | { readonly [key: string]: Jsonable }
+    | { toJSON(): Jsonable }
+
+class BaseError extends Error {
+    public readonly context?: Jsonable
+
+    constructor(
+        message: string,
+        options: { error?: Error; context?: Jsonable } = {},
+    ) {
+        const { cause, context } = options
+
+        super(message, { cause })
+        this.name = this.constructor.name
+
+        this.context = context
+    }
+}
