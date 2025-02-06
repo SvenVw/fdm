cd9501e46f2f57567062c7cf2ac16cf4a4a30df6
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-06 17:01:03 +0100

    Add save to db for harvest

--- fdm-app/app/routes/farm.create.$b_id_farm.cultivations.$b_lu_catalogue.crop.tsx
+++ fdm-app/app/routes/farm.create.$b_id_farm.cultivations.$b_lu_catalogue.crop.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
-import { Form, useNavigation } from "react-router"
+import {
+    type ActionFunctionArgs,
+    data,
+    Form,
+    type LoaderFunctionArgs,
+    useLoaderData,
+} from "react-router"
 import { Calendar } from "@/components/ui/calendar"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { z } from "zod"
 import { RemixFormProvider, useRemixForm } from "remix-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -24,23 +28,77 @@ import {
 import { cn } from "@/lib/utils"
 import { format } from "date-fns"
 import { CalendarIcon } from "lucide-react"
+import { nl } from "date-fns/locale"
+import {
+    addHarvest,
+    getCultivationPlan,
+    updateCultivation,
+} from "@svenvw/fdm-core"
+import { extractFormValuesFromRequest } from "@/lib/form"
+import { dataWithSuccess } from "remix-toast"
+import { fdm } from "@/lib/fdm.server"
 
 const FormSchema = z.object({
-    b_lu_catalogue: z.coerce.string(),
-    b_lu_yield: z.coerce.number(),
-    b_lu_n_harvestable: z.coerce.number(),
-    b_sowing_date: z.coerce.date(),
-    b_harvesting_date: z.coerce.date(),
+    b_lu_yield: z.coerce
+        .number({
+            invalid_type_error: "Hoeveelheid moet een getal zijn",
+        })
+        .positive({
+            message: "Hoeveelheid moet positief zijn",
+        })
+        .finite({
+            message: "Hoeveelheid moet een geheel getal zijn",
+        })
+        .max(100, {
+            message: "Hoeveelheid mag niet groter zijn dan 100",
+        })
+        .safe({
+            message: "Hoeveelheid moet een safe getal zijn",
+        })
+        .optional(),
+    b_lu_n_harvestable: z.coerce
+        .number({
+            invalid_type_error: "Hoeveelheid moet een getal zijn",
+        })
+        .positive({
+            message: "Hoeveelheid moet positief zijn",
+        })
+        .finite({
+            message: "Hoeveelheid moet een geheel getal zijn",
+        })
+        .max(1000, {
+            message: "Hoeveelheid mag niet groter zijn dan 1000",
+        })
+        .safe({
+            message: "Hoeveelheid moet een safe getal zijn",
+        })
+        .optional(),
+    b_sowing_date: z.coerce.date().optional(),
+    b_harvesting_date: z.coerce.date().optional(),
 })
 
-export default function Index(props: { action: string | undefined }) {
-    const navigation = useNavigation()
+export async function loader({ request, params }: LoaderFunctionArgs) {
+    const b_lu_catalogue = params.b_lu_catalogue
+    if (!b_lu_catalogue) {
+        throw new Error("b_lu_catalogue is required")
+    }
+
+    return {
+        b_lu_catalogue: b_lu_catalogue,
+    }
+}
+
+export default function FarmAFieldCultivationBlock() {
+    const loaderData = useLoaderData<typeof loader>()
 
     const form = useRemixForm<z.infer<typeof FormSchema>>({
         mode: "onTouched",
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            b_lu_catalogue: "nl_265",
+            b_lu_yield: undefined,
+            b_lu_n_harvestable: undefined,
+            b_sowing_date: undefined,
+            b_harvesting_date: undefined,
         },
     })
 
@@ -124,7 +182,7 @@ export default function Index(props: { action: string | undefined }) {
                                                         {field.value ? (
                                                             format(
                                                                 field.value,
-                                                                "PPP",
+                                                                "yyyy-MM-dd",
                                                             )
                                                         ) : (
                                                             <span>
@@ -140,6 +198,7 @@ export default function Index(props: { action: string | undefined }) {
                                                 align="start"
                                             >
                                                 <Calendar
+                                                    locale={nl}
                                                     mode="single"
                                                     selected={field.value}
                                                     onSelect={field.onChange}
@@ -178,7 +237,7 @@ export default function Index(props: { action: string | undefined }) {
                                                         {field.value ? (
                                                             format(
                                                                 field.value,
-                                                                "PPP",
+                                                                "yyyy-MM-dd",
                                                             )
                                                         ) : (
                                                             <span>
@@ -194,6 +253,7 @@ export default function Index(props: { action: string | undefined }) {
                                                 align="start"
                                             >
                                                 <Calendar
+                                                    locale={nl}
                                                     mode="single"
                                                     selected={field.value}
                                                     onSelect={field.onChange}
@@ -227,5 +287,71 @@ export default function Index(props: { action: string | undefined }) {
                 </Form>
             </RemixFormProvider>
         </div>
+    )
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
+    const b_lu_catalogue = params.b_lu_catalogue
+    if (!b_lu_catalogue) {
+        throw new Error("b_lu_catalogue is required")
+    }
+    const b_id_farm = params.b_id_farm
+    if (!b_id_farm) {
+        throw new Error("b_id_farm is required")
+    }
+
+    // Get cultivation id's for this cultivation code
+    const cultivationPlan = await getCultivationPlan(fdm, b_id_farm)
+    const cultivation = cultivationPlan.find(
+        (cultivation) => cultivation.b_lu_catalogue === b_lu_catalogue,
+    )
+    const b_lu = cultivation.fields.map((field) => field.b_lu)
+    const formValues = await extractFormValuesFromRequest(request, FormSchema)
+    console.log(formValues)
+
+    // Add cultivation details for each cultivation
+    await Promise.all(
+        b_lu.map(async (item) => {
+            try {
+                if (formValues.b_sowing_date) {
+                    await updateCultivation(
+                        fdm,
+                        item,
+                        undefined,
+                        formValues.b_sowing_date,
+                    )
+                }
+                if (
+                    formValues.b_harvesting_date ||
+                    formValues.b_lu_yield ||
+                    formValues.b_lu_n_harvestable
+                ) {
+                    await addHarvest(
+                        fdm,
+                        item,
+                        formValues.b_harvesting_date,
+                        formValues.b_lu_yield,
+                        formValues.b_lu_n_harvestable,
+                    )
+                }
+            } catch (error) {
+                console.error(
+                    `Failed to process cultivation ${b_lu_catalogue} for farm ${b_id_farm}:`,
+                    error,
+                )
+                throw data(
+                    `Failed to process cultivation ${b_lu_catalogue} for farm ${b_id_farm}: ${error.message}`,
+                    {
+                        status: 500,
+                        statusText: `Failed to process cultivation ${b_lu_catalogue} for farm ${b_id_farm}`,
+                    },
+                )
+            }
+        }),
+    )
+
+    return dataWithSuccess(
+        { result: "Data saved successfully" },
+        "Gegegevens zijn succesvol opgeslagen! 🎉",
     )
 }
