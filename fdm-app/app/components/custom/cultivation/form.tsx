7f519b91865eb9104eecb6129bcf4cb82e53865d
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-11 16:42:05 +0100

    Add cultivationForm component

---
+++ fdm-app/app/components/custom/cultivation/form.tsx
@@ -0,0 +1,202 @@
+import { useEffect } from "react"
+import { Form } from "react-router"
+import { RemixFormProvider, useRemixForm } from "remix-hook-form"
+import { FormSchema } from "./schema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import type { CultivationsFormProps } from "./types"
+import { Combobox } from "../combobox"
+import type { z } from "zod"
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { format } from "date-fns/format"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import { LoadingSpinner } from "../loadingspinner"
+
+export function CultivationsForm({
+    b_lu_catalogue,
+    b_sowing_date,
+    b_terminating_date,
+    options,
+    action,
+}: CultivationsFormProps) {
+    const form = useRemixForm<z.infer<typeof FormSchema>>({
+        mode: "onTouched",
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            b_lu_catalogue: b_lu_catalogue,
+            b_sowing_date: b_sowing_date,
+            b_terminating_date: b_terminating_date,
+        },
+    })
+
+    useEffect(() => {
+        if (form.formState.isSubmitSuccessful) {
+            form.reset()
+        }
+    }, [form.formState, form.reset])
+
+    return (
+        <RemixFormProvider {...form}>
+            <Form
+                id="formCultivation"
+                action={action}
+                onSubmit={form.handleSubmit}
+                method="POST"
+            >
+                <fieldset disabled={form.formState.isSubmitting}>
+                    <div className="grid grid-cols-5 items-end gap-x-3 justify-between">
+                        <div className="col-span-2">
+                            <Combobox
+                                options={options}
+                                form={form}
+                                name="b_lu_catalogue"
+                                label={
+                                    <span>
+                                        Gewas
+                                        <span className="text-red-500">*</span>
+                                    </span>
+                                }
+                                disabled={!!b_lu_catalogue}
+                            />
+                        </div>
+                        <div>
+                            <FormField
+                                control={form.control}
+                                name="b_sowing_date"
+                                render={({ field }) => (
+                                    <FormItem className="">
+                                        <FormLabel>Zaaidatum</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-full text-left font-normal",
+                                                            !field.value &&
+                                                                "text-muted-foreground",
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(
+                                                                field.value,
+                                                                "yyyy-MM-dd",
+                                                            )
+                                                        ) : (
+                                                            <span>
+                                                                Kies een datum
+                                                            </span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent
+                                                className="w-auto p-0"
+                                                align="start"
+                                            >
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    // disabled={(date) =>
+                                                    //     date > new Date() || date < new Date("1900-01-01")
+                                                    // }
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription />
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div>
+                            <FormField
+                                control={form.control}
+                                name="b_terminating_date"
+                                render={({ field }) => (
+                                    <FormItem className="">
+                                        <FormLabel>Einddatum</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-full text-left font-normal",
+                                                            !field.value &&
+                                                                "text-muted-foreground",
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(
+                                                                field.value,
+                                                                "yyyy-MM-dd",
+                                                            )
+                                                        ) : (
+                                                            <span>
+                                                                Kies een datum
+                                                            </span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent
+                                                className="w-auto p-0"
+                                                align="start"
+                                            >
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    // disabled={(date) =>
+                                                    //     date > new Date() || date < new Date("1900-01-01")
+                                                    // }
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription />
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <div className="justify-self-end">
+                            <Button type="submit">
+                                {form.formState.isSubmitting ? (
+                                    <div className="flex items-center space-x-2">
+                                        <LoadingSpinner />
+                                        <span>Opslaan...</span>
+                                    </div>
+                                ) : b_lu_catalogue ? (
+                                    "Bijwerken"
+                                ) : (
+                                    "Voeg toe"
+                                )}
+                            </Button>
+                        </div>
+                    </div>
+                </fieldset>
+            </Form>
+        </RemixFormProvider>
+    )
+}
---
+++ fdm-app/app/components/custom/cultivation/schema.tsx
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const FormSchema = z.object({
+    b_lu_catalogue: z.string({
+        required_error: "Gewas is verplicht",
+    }),
+    b_sowing_date: z.coerce.date({
+        required_error: "Datum is verplicht",
+        invalid_type_error: "Datum is ongeldig",
+    }),
+    b_terminating_date: z.coerce
+        .date({
+            invalid_type_error: "Datum is ongeldig",
+        })
+        .optional(),
+})
