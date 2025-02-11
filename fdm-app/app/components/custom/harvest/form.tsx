c4e17c8b5397c394765730ddf2119799d64a87e5
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-11 12:58:30 +0100

    Add form to add harvests

---
+++ fdm-app/app/components/custom/harvest/form.tsx
@@ -0,0 +1,234 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { RemixFormProvider, useRemixForm } from "remix-hook-form"
+import type { z } from "zod"
+import { FormSchema } from "./schema"
+import { Form } from "react-hook-form"
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { format } from "date-fns/format"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import { LoadingSpinner } from "../loadingspinner"
+import { nl } from "date-fns/locale/nl"
+
+export function HarvestForm({
+    b_lu_yield,
+    b_lu_n_harvestable,
+    b_harvesting_date,
+}: {b_lu_yield: number | undefined, b_lu_n_harvestable: number | undefined, b_harvesting_date: Date | undefined}) {
+    const form = useRemixForm<z.infer<typeof FormSchema>>({
+        mode: "onTouched",
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            b_lu_yield: b_lu_yield,
+            b_lu_n_harvestable: b_lu_n_harvestable,
+            b_harvesting_date: b_harvesting_date,
+        },
+    })
+
+    // Currently updateHarvest function is not available, therefore check if this is a new harvest or is has already values
+    const isHarvestUpdate = b_lu_yield !== undefined || b_lu_n_harvestable !== undefined || b_harvesting_date !== undefined
+
+    return (
+        <div className="space-y-6">
+            {/* <div>
+                <p className="text-sm text-muted-foreground">
+                    Werk de opbrengst, stikstofgehalte en zaai- en oogstdatum
+                    bij voor dit gewas.
+                </p>
+            </div> */}
+            <RemixFormProvider {...form}>
+                <Form
+                    id="formCultivation"
+                    onSubmit={form.handleSubmit}
+                    method="POST"
+                >
+                    <fieldset disabled={form.formState.isSubmitting || isHarvestUpdate} className="space-y-8">
+                        <div className="grid w-4/5 lg:grid-cols-2 items-center gap-y-6 gap-x-8">
+                            <FormField
+                                control={form.control}
+                                name="b_lu_yield"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>
+                                            Opbrengst (droge stof)
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Bv. 37 ton ds / ha"
+                                                aria-required="true"
+                                                type="number"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+                                            In ton droge stof per hectare
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="b_lu_n_harvestable"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Stikstofgehalte</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Bv. 4 kg N / ha"
+                                                aria-required="true"
+                                                type="number"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+                                            In geoogst product (kg N / ha)
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            {/* <FormField
+                                control={form.control}
+                                name="b_sowing_date"                    
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Zaaidatum</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-[240px] pl-3 text-left font-normal",
+                                                            !field.value &&
+                                                                "text-muted-foreground",
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(
+                                                                field.value,
+                                                                "yyyy-MM-dd",
+                                                            )
+                                                        ) : (
+                                                            <span>
+                                                                Kies een datum
+                                                            </span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent
+                                                className="w-auto p-0"
+                                                align="start"
+                                            >
+                                                <Calendar
+                                                    locale={nl}
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    disabled={(date) =>
+                                                        date <
+                                                        new Date("1970-01-01")
+                                                    }
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription>
+                                            Kan ook poot- of aanplantdatum zijn
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            /> */}
+                            <FormField
+                                control={form.control}
+                                name="b_harvesting_date"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Oogstdatum</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-[240px] pl-3 text-left font-normal",
+                                                            !field.value &&
+                                                                "text-muted-foreground",
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(
+                                                                field.value,
+                                                                "yyyy-MM-dd",
+                                                            )
+                                                        ) : (
+                                                            <span>
+                                                                Kies een datum
+                                                            </span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent
+                                                className="w-auto p-0"
+                                                align="start"
+                                            >
+                                                <Calendar
+                                                    locale={nl}
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    disabled={(date) =>
+                                                        date <
+                                                        new Date("1970-01-01")
+                                                    }
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription>
+                                            Kan ook inwerkdatum zijn
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div>
+                            <Button type="submit">
+                                {form.formState.isSubmitting ? (
+                                    <div className="flex items-center space-x-2">
+                                        <LoadingSpinner />
+                                        <span>Opslaan...</span>
+                                    </div>
+                                ) : (
+                                    "Opslaan"
+                                )}
+                            </Button>
+                        </div>
+                    </fieldset>
+                </Form>
+            </RemixFormProvider>
+        </div>
+    )
+}
---
+++ fdm-app/app/components/custom/harvest/schema.tsx
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+export const FormSchema = z.object({
+    b_lu_yield: z.coerce
+        .number({
+            invalid_type_error: "Hoeveelheid moet een getal zijn",
+        })
+        .positive({
+            message: "Hoeveelheid moet positief zijn",
+        })
+        .finite({
+            message: "Hoeveelheid moet een geheel getal zijn",
+        })
+        .max(100, {
+            message: "Hoeveelheid mag niet groter zijn dan 100",
+        })
+        .safe({
+            message: "Hoeveelheid moet een safe getal zijn",
+        })
+        .optional(),
+    b_lu_n_harvestable: z.coerce
+        .number({
+            invalid_type_error: "Hoeveelheid moet een getal zijn",
+        })
+        .positive({
+            message: "Hoeveelheid moet positief zijn",
+        })
+        .finite({
+            message: "Hoeveelheid moet een geheel getal zijn",
+        })
+        .max(1000, {
+            message: "Hoeveelheid mag niet groter zijn dan 1000",
+        })
+        .safe({
+            message: "Hoeveelheid moet een safe getal zijn",
+        })
+        .optional(),
+    // b_sowing_date: z.coerce.date().optional(),
+    b_harvesting_date: z.coerce.date().optional(),
+})
