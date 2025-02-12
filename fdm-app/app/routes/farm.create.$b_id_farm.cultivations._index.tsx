d61a487656fc3b8c4c2bbd95b575633d4a139154
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-12 13:27:09 +0100

    Add redirect to first cultivation

---
+++ fdm-app/app/routes/farm.create.$b_id_farm.cultivations._index.tsx
@@ -0,0 +1,13 @@
+import { fdm } from "@/lib/fdm.server"
+import { getCultivationPlan } from "@svenvw/fdm-core"
+import { type LoaderFunctionArgs, redirect } from "react-router"
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+    // Redirect to first cultivation
+    const b_id_farm = params.b_id_farm
+    if (!b_id_farm) {
+        throw new Error("b_id_farm is required")
+    }
+    const cultivationPlan = await getCultivationPlan(fdm, b_id_farm)
+    return redirect(`${cultivationPlan[0].b_lu_catalogue}/crop`)
+}
