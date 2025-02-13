e518d78a2c3950f2814059c423f2ace60f94d058
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-13 17:00:30 +0100

    Export the interfaces `getFertilizerType` and `getFertilizerApplicationType`

--- fdm-core/src/index.ts
+++ fdm-core/src/index.ts
@@ -34,6 +34,7 @@ export {
     getFertilizerApplication,
     getFertilizerApplications,
 } from "./fertilizer"
+export type {getFertilizerType, getFertilizerApplicationType} from "./fertilizer.d"
 export {
     addCultivationToCatalogue,
     getCultivationsFromCatalogue,
