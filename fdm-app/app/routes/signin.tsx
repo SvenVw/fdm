e4dbdfcb6811f4e9e869b44ff213d6a7a0f3b71e
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-21 15:18:42 +0100

    Optimize background image loading

--- fdm-app/app/routes/signin.tsx
+++ fdm-app/app/routes/signin.tsx
@@ -246,6 +246,7 @@ export default function SignIn() {
                     alt=""
                     width="1920"
                     height="1080"
+                    loading="lazy"
                     className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
                 />
             </div>
