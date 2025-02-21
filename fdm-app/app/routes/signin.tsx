a255c9d828620c4fcca99f0d3468cdd5598e87b9
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-21 14:29:24 +0100

    improve styling

--- fdm-app/app/routes/signin.tsx
+++ fdm-app/app/routes/signin.tsx
@@ -135,15 +135,29 @@ export default function SignIn() {
                                         }}
                                     >
                                         <svg
+                                            width="1024"
+                                            height="1024"
+                                            viewBox="0 0 1024 1024"
+                                            fill="none"
                                             xmlns="http://www.w3.org/2000/svg"
-                                            width="1em"
-                                            height="1em"
-                                            viewBox="0 0 24 24"
                                         >
+                                            <title>Microsoft logo</title>
                                             <path
-                                                fill="currentColor"
-                                                d="M2 3h9v9H2zm9 19H2v-9h9zM21 3v9h-9V3zm0 19h-9v-9h9z"
-                                            ></path>
+                                                d="M44.522 44.5217H489.739V489.739H44.522V44.5217Z"
+                                                fill="#F35325"
+                                            />
+                                            <path
+                                                d="M534.261 44.5217H979.478V489.739H534.261V44.5217Z"
+                                                fill="#81BC06"
+                                            />
+                                            <path
+                                                d="M44.522 534.261H489.739V979.478H44.522V534.261Z"
+                                                fill="#05A6F0"
+                                            />
+                                            <path
+                                                d="M534.261 534.261H979.478V979.478H534.261V534.261Z"
+                                                fill="#FFBA08"
+                                            />
                                         </svg>
                                         Aanmelden met Microsoft
                                     </Button>
@@ -172,32 +186,33 @@ export default function SignIn() {
                                             height="1em"
                                             viewBox="0 0 256 262"
                                         >
+                                            <title>Google logo</title>
                                             <path
                                                 fill="#4285F4"
                                                 d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-                                            ></path>
+                                            />
 
                                             <path
                                                 fill="#34A853"
                                                 d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-                                            ></path>
+                                            />
 
                                             <path
                                                 fill="#FBBC05"
                                                 d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
-                                            ></path>
+                                            />
 
                                             <path
                                                 fill="#EB4335"
                                                 d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-                                            ></path>
+                                            />
                                         </svg>
                                         Aanmelden met Google
                                     </Button>
                                 </div>
                             </div>
                         </CardContent>
-                        <CardFooter className="flex justify-between">
+                        <CardFooter className="flex justify-center">
                             <p className="text-sm font-medium text-muted-foreground text-center">
                                 Door verder te gaan, gaat u akkoord met het{" "}
                                 <a href="/privacy" className="underline">
@@ -205,7 +220,7 @@ export default function SignIn() {
                                 </a>
                             </p>
                         </CardFooter>
-                    </Card>                   
+                    </Card>
                     <div className="mb-4 text-center text-sm">
                         <Button variant={"outline"}>
                             Lees meer over FDM <MoveDown />
