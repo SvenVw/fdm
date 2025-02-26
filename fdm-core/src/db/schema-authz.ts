3ce03642f88592872e1e2699a7bf99baca43362e
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-02-26 17:28:19 +0100

    Add index to role table

--- fdm-core/src/db/schema-authz.ts
+++ fdm-core/src/db/schema-authz.ts
@@ -1,18 +1,20 @@
 // Authorization
-import { boolean, pgSchema, text, timestamp } from "drizzle-orm/pg-core"
-import { roles, resources, actions } from "../authorization"
+import { boolean, index, pgSchema, text, timestamp } from "drizzle-orm/pg-core"
 
 // Define postgres schema
 export const fdmAuthZSchema = pgSchema("fdm-authz")
 export type fdmSchemaAuthZTypeSelect = typeof fdmAuthZSchema
 
 export const role = fdmAuthZSchema.table("role", {
-    resource: text(),
-    resource_id: text(),
-    principal_id: text(),
-    role: text(),
+    role_id: text().primaryKey(),
+    resource: text().notNull(),
+    resource_id: text().notNull(),
+    principal_id: text().notNull(),
+    role: text().notNull(),
     created: timestamp({ withTimezone: true }).notNull().defaultNow(),
     deleted: timestamp({ withTimezone: true }),
+}, (table) => {
+    index("role_idx").on(table.resource, table.resource_id, table.principal_id, table.role, table.deleted)
 })
 
 export type roleTypeSelect = typeof role.$inferSelect
@@ -21,11 +23,11 @@ export type roleTypeInsert = typeof role.$inferInsert
 export const audit = fdmAuthZSchema.table("audit", {
     audit_id: text().primaryKey(),
     audit_timestamp: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    principal_id: text(),
-    resource: text(),
-    resource_id: text(),
-    action: text(),
-    allowed: boolean(),
+    principal_id: text().notNull(),
+    resource: text().notNull(),
+    resource_id: text().notNull(),
+    action: text().notNull(),
+    allowed: boolean().notNull(),
 })
 
 export type auditTypeSelect = typeof audit.$inferSelect
