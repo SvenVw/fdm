917b36bd9089b2035c7e549645db82e4a6d0e9e8
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-03-14 14:10:45 +0100

    Improve bundle by manually chunking

--- fdm-app/vite.config.ts
+++ fdm-app/vite.config.ts
@@ -20,5 +20,32 @@ export default defineConfig({
     build: {
         sourcemap: process.env.NODE_ENV === "development",
         target: "ES2022",
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return "package"
+                    }
+                    if (id.includes("app/components/ui")) {
+                        return "components/ui"
+                    }
+                    if (id.includes("app/components/custom")) {
+                        return "components/custom"
+                    }
+                    if (id.includes("app/lib")) {
+                        return "lib"
+                    }
+                    if (id.includes("app/store")) {
+                        return "store"
+                    }
+                    if (id.includes("app/hooks")) {
+                        return "hooks"
+                    }
+                    if (id.includes("public")) {
+                        return "public"
+                    }
+                },
+            },
+        },
     },
 })
