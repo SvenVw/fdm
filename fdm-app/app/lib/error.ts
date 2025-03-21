0318a4cedfed4c86dd01618cf3e0b6344f4d9708
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-03-21 09:30:12 +0100

    At 401 error redirect user to signin page

--- fdm-app/app/lib/error.ts
+++ fdm-app/app/lib/error.ts
@@ -1,4 +1,4 @@
-import { data } from "react-router"
+import { data, redirect } from "react-router"
 import { dataWithError, dataWithWarning } from "remix-toast"
 
 export function handleLoaderError(error: unknown) {
@@ -23,9 +23,7 @@ export function handleLoaderError(error: unknown) {
                     userMessage = error.statusText
                     break
                 case 401:
-                    userMessage =
-                        "U bent niet geautoriseerd om deze actie uit te voeren."
-                    break
+                    return redirect("/signin")
                 case 403:
                     userMessage =
                         "U heeft geen rechten om deze actie uit te voeren."
@@ -135,10 +133,7 @@ export function handleActionError(error: unknown) {
                     dataStatus = "warning"
                     break
                 case 401:
-                    userMessage =
-                        "U bent niet geautoriseerd om deze actie uit te voeren."
-                    dataStatus = "warning"
-                    break
+                    return redirect("/signin")
                 case 403:
                     userMessage =
                         "U heeft geen rechten om deze actie uit te voeren."
