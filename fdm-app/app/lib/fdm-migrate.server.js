58822a2c867f534bf5ee06c09f8956199da20b41
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-04-03 14:15:25 +0200

    Use config for db connection

--- fdm-app/app/lib/fdm-migrate.server.js
+++ fdm-app/app/lib/fdm-migrate.server.js
@@ -5,33 +5,35 @@ import {
 } from "@svenvw/fdm-core"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
+import { serverConfig } from "~/lib/config"
 
 // Get credentials to connect to db
 const host =
-    process.env.POSTGRES_HOST ??
+    serverConfig.database.host ??
     (() => {
         throw new Error("POSTGRES_HOST environment variable is required")
     })()
 const port =
-    Number(process.env.POSTGRES_PORT) ||
+    serverConfig.database.port ||
     (() => {
         throw new Error("POSTGRES_PORT environment variable is required")
     })()
 const user =
-    process.env.POSTGRES_USER ??
+    serverConfig.database.user ??
     (() => {
         throw new Error("POSTGRES_USER environment variable is required")
     })()
 const password =
-    process.env.POSTGRES_PASSWORD ??
+    serverConfig.database.password ??
     (() => {
         throw new Error("POSTGRES_PASSWORD environment variable is required")
     })()
 const database =
-    process.env.POSTGRES_DB ??
+    serverConfig.database.database ??
     (() => {
         throw new Error("POSTGRES_DB environment variable is required")
     })()
+
 const migrationsFolderPath = "node_modules/@svenvw/fdm-core/dist/db/migrations"
 
 const client = postgres({
--- fdm-app/app/lib/fdm.server.tsx
+++ fdm-app/app/lib/fdm.server.tsx
@@ -1,29 +1,30 @@
 import { fdmSchema as schema } from "@svenvw/fdm-core"
 import { drizzle } from "drizzle-orm/postgres-js"
+import { serverConfig } from "~/lib/config"
 
 // Get credentials to connect to db
 const host =
-    process.env.POSTGRES_HOST ??
+    serverConfig.database.host ??
     (() => {
         throw new Error("POSTGRES_HOST environment variable is required")
     })()
 const port =
-    Number(process.env.POSTGRES_PORT) ||
+    serverConfig.database.port ||
     (() => {
         throw new Error("POSTGRES_PORT environment variable is required")
     })()
 const user =
-    process.env.POSTGRES_USER ??
+    serverConfig.database.user ??
     (() => {
         throw new Error("POSTGRES_USER environment variable is required")
     })()
 const password =
-    process.env.POSTGRES_PASSWORD ??
+    serverConfig.database.password ??
     (() => {
         throw new Error("POSTGRES_PASSWORD environment variable is required")
     })()
 const database =
-    process.env.POSTGRES_DB ??
+    serverConfig.database.database ??
     (() => {
         throw new Error("POSTGRES_DB environment variable is required")
     })()
