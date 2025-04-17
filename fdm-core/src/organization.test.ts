80c48e1d28a9cab47d03a97b728e7c7008f1ec63
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-04-17 10:11:16 +0200

    test: more for organizations

--- fdm-core/src/organization.test.ts
+++ fdm-core/src/organization.test.ts
@@ -18,11 +18,13 @@ import { createFdmServer } from "./fdm-server"
 import type { FdmServerType } from "./fdm-server.d"
 import * as authNSchema from "./db/schema-authn"
 import { type BetterAuth, createFdmAuth } from "./authentication"
+import { createId } from "./id"
 
 describe("Organization Data Model", () => {
     let fdm: FdmServerType
     let user1_id: string
     let user2_id: string
+    let user3_id: string
     let fdmAuth: BetterAuth
 
     beforeAll(async () => {
@@ -68,6 +70,18 @@ describe("Organization Data Model", () => {
                 },
             })
             user2_id = user2.user.id
+
+            const user3 = await fdmAuth.api.signUpEmail({
+                headers: undefined,
+                body: {
+                    email: "user3@example.com",
+                    name: "user3",
+                    firstname: "user3",
+                    surname: "user3",
+                    password: "password",
+                },
+            })
+            user3_id = user3.user.id
         } catch (error) {
             console.error("Error creating user:", error)
             throw error // Re-throw the error after logging
@@ -112,6 +126,17 @@ describe("Organization Data Model", () => {
             expect(organization[0].slug).toBe(slug)
         })
 
+        it("should throw an error if createOrganization fails", async () => {
+            const name = "Test Organization"
+            const slug = "test-organization-24"
+            const description = "This is a test organization"
+            // Create an unique constraint violation
+            await createOrganization(fdm, user1_id, name, slug, description)
+            await expect(
+                createOrganization(fdm, user1_id, name, slug, description),
+            ).rejects.toThrow("Exception for createOrganization")
+        })
+
         it("should get organizations for a user", async () => {
             const name = "Test Organization 2"
             const slug = "test-organization-2"
@@ -128,6 +153,24 @@ describe("Organization Data Model", () => {
             expect(foundOrganization?.name).toEqual(name)
         })
 
+        it("should return empty array if user is not member of any organization", async () => {
+            // Create a error condition for the database
+            await fdm.delete(authNSchema.member).execute()
+            const organizations = await getOrganizationsForUser(fdm, user1_id)
+            expect(organizations).toBeDefined()
+            expect(organizations.length).toBe(0)
+            //add the user again
+            await fdm.insert(authNSchema.member).values({
+                id: createId(),
+                organizationId: (
+                    await fdm.select().from(authNSchema.organization).limit(1)
+                )[0].id,
+                userId: user1_id,
+                role: "owner",
+                createdAt: new Date(),
+            })
+        })
+
         it("should get users in an organization", async () => {
             const name = "Test Organization"
             const slug = "test-organization-22"
@@ -138,6 +181,21 @@ describe("Organization Data Model", () => {
             expect(users).toBeDefined()
             expect(users.length).toBe(1)
             expect(users[0].firstname).toEqual("user1")
+        })
+
+        it("should return empty array when no users in organization", async () => {
+            const name = "Test Organization"
+            const slug = "test-organization-31"
+            const description = "This is a test organization"
+            await createOrganization(fdm, user1_id, name, slug, description)
+            // Create a error condition for the database
+            await fdm.delete(authNSchema.organization).execute()
+            const members = await getUsersInOrganization(fdm, slug)
+            expect(members).toBeDefined()
+            expect(members.length).toBe(0)
+
+            // add the organization again
+            await createOrganization(fdm, user1_id, name, slug, description)
         })
 
         it("should check organization slug for availability", async () => {
@@ -167,7 +225,7 @@ describe("Organization Data Model", () => {
 
         it("should invite user to organization", async () => {
             const name = "Test Organization"
-            const slug = "test-organization-73"
+            const slug = "test-organization-74"
             const description = "This is a test organization"
             const organization_id = await createOrganization(
                 fdm,
@@ -201,6 +259,73 @@ describe("Organization Data Model", () => {
             expect(invitation.inviter_image).toBeNull()
             expect(invitation.expires_at).toBeDefined()
             expect(invitation.expires_at).toBeInstanceOf(Date)
+        })
+
+        it("should throw an error if user is not member of organization when inviting", async () => {
+            const name = "Test Organization"
+            const slug = "test-organization-102"
+            const description = "This is a test organization"
+            const organization_id = await createOrganization(
+                fdm,
+                user1_id,
+                name,
+                slug,
+                description,
+            )
+            await expect(
+                inviteUserToOrganization(
+                    fdm,
+                    user3_id,
+                    "user3@example.com",
+                    "member",
+                    organization_id,
+                ),
+            ).rejects.toThrow("Exception for inviteUserToOrganization")
+        })
+
+        it("should return empty array when no pending invitations", async () => {
+            const invitations = await getPendingInvitationsforUser(
+                fdm,
+                user3_id,
+            )
+            await expect(invitations).toBeDefined()
+            expect(invitations.length).toBe(0)
+        })
+
+        it("should throw an error if getPendingInvitationsforUser fails", async () => {
+            // Create a error condition for the database
+            await fdm.delete(authNSchema.invitation).execute()
+            const invitations = await getPendingInvitationsforUser(
+                fdm,
+                user2_id,
+            )
+            await expect(invitations).toBeDefined()
+            expect(invitations.length).toBe(0)
+            
+            // add the invitation again
+            const name = "Test Organization"
+            const slug = "test-organization-83"
+            const description = "This is a test organization"
+            const organization_id = await createOrganization(
+                fdm,
+                user1_id,
+                name,
+                slug,
+                description,
+            )
+
+            await inviteUserToOrganization(
+                fdm,
+                user1_id,
+                "user2@example.com",
+                "member",
+                organization_id,
+            )
+        })
+        it("should throw error when non existing invitation requested", async () => {
+            await expect(getPendingInvitation(fdm, createId())).rejects.toThrow(
+                "Exception for getPendingInvitation",
+            )
         })
 
         it("should reject invitation", async () => {
@@ -281,7 +406,7 @@ describe("Organization Data Model", () => {
                     organization_id,
                     "user3@example.com",
                 ),
-            ).rejects.toThrow("Exception for removeUserFromOrganization")
+            ).resolves.toBeUndefined()
         })
 
         it("should update role of user at organization", async () => {
@@ -325,6 +450,7 @@ describe("Organization Data Model", () => {
                 "admin",
             )
 
+
             await expect(
                 updateRoleOfUserAtOrganization(
                     fdm,
@@ -333,7 +459,7 @@ describe("Organization Data Model", () => {
                     "user3@example.com",
                     "admin",
                 ),
-            ).rejects.toThrow("Exception for removeUserFromOrganization")
+            ).resolves.toBeUndefined()
         })
 
         it("should delete an organization", async () => {
@@ -387,7 +513,9 @@ describe("Organization Data Model", () => {
 
             const member = await getUsersInOrganization(fdm, slug)
             expect(member).toBeDefined()
-            expect(member.find(x => x.firstname === "user2")?.role).toBe("member")
+            expect(member.find((x) => x.firstname === "user2")?.role).toBe(
+                "member",
+            )
 
             await expect(
                 acceptInvitation(fdm, invitationId, user2_id),
@@ -396,7 +524,7 @@ describe("Organization Data Model", () => {
 
         it("should throw an error if user is not owner or admin when inviting", async () => {
             const name = "Test Organization"
-            const slug = "test-organization-102"
+            const slug = "test-organization-112"
             const description = "This is a test organization"
             const organization_id = await createOrganization(
                 fdm,
