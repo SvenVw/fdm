ed226afade47d94b86e83241bb836bf99e83669b
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-04-28 17:24:13 +0200

    feat: On the farm selection page show for every farm a card

---
+++ .changeset/ten-files-camp.md
@@ -0,0 +1,5 @@
+---
+"@svenvw/fdm-app": minor
+---
+
+: Redesign the page on which the farm can be selected by providing a card with some details for every farm
--- fdm-app/app/routes/farm._index.tsx
+++ fdm-app/app/routes/farm._index.tsx
@@ -14,6 +14,7 @@ import {
     CardDescription,
     CardFooter,
     CardHeader,
+    CardTitle,
 } from "~/components/ui/card"
 import { Separator } from "~/components/ui/separator"
 import { SidebarInset } from "~/components/ui/sidebar"
@@ -22,6 +23,7 @@ import { clientConfig } from "~/lib/config"
 import { handleLoaderError } from "~/lib/error"
 import { fdm } from "~/lib/fdm.server"
 import { getTimeBasedGreeting } from "~/lib/greetings"
+import { Badge } from "../components/ui/badge"
 
 // Meta
 export const meta: MetaFunction = () => {
@@ -34,14 +36,23 @@ export const meta: MetaFunction = () => {
     ]
 }
 
+// Define a type for farm with added role property
+type Farm = {
+    b_id_farm: string
+    b_name_farm: string
+    b_businessid_farm: string
+    b_address_farm: string
+    b_postalcode_farm: string
+}
+
 /**
- * Retrieves the user session and associated farms data.
+ * Retrieves the user session and associated farms data, including the user's role.
  *
- * The function obtains the user session from the incoming request and then fetches the user's farms using the session's principal ID. It maps the farm data into a simplified array containing each farm's identifier and name, and returns this alongside the user's name.
+ * The function obtains the user session from the incoming request and then fetches the user's farms using the session's principal ID. It maps the farm data into a simplified array containing each farm's identifier, name, and the user's role. It returns this alongside the user's name.
  *
  * @param request - The HTTP request object used to retrieve session information.
  * @returns An object containing:
- *   - farmOptions: An array of objects, each with a farm's ID and name.
+ *   - farmsWithRoles: An array of objects, each with a farm's ID, name, and the user's role.
  *   - username: The user's name from the session data.
  *
  * @throws {Error} If retrieving the session or fetching the farm data fails.
@@ -54,16 +65,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
         // Get a list of possible farms of the user
         const farms = await getFarms(fdm, session.principal_id)
 
-        const farmOptions = farms.map((farm) => {
-            return {
-                b_id_farm: farm.b_id_farm,
-                b_name_farm: farm.b_name_farm,
-            }
-        })
-
         // Return user information from loader
         return {
-            farmOptions: farmOptions,
+            farms: farms,
             username: session.userName,
         }
     } catch (error) {
@@ -85,12 +89,15 @@ export default function AppIndex() {
     return (
         <SidebarInset>
             <FarmHeader
-                farmOptions={loaderData.farmOptions}
+                farmOptions={loaderData.farms.map((farm) => ({
+                    b_id_farm: farm.b_id_farm,
+                    b_name_farm: farm.b_name_farm,
+                }))}
                 b_id_farm={undefined}
                 action={undefined}
             />
             <main>
-                {loaderData.farmOptions.length === 0 ? (
+                {loaderData.farms.length === 0 ? (
                     <>
                         <FarmTitle
                             title={`Welkom, ${loaderData.username}! 👋`}
@@ -125,62 +132,94 @@ export default function AppIndex() {
                                 "Kies een bedrijf uit de lijst om verder te gaan of maak een nieuw bedrijf aan"
                             }
                         />
-                        <div className="flex h-full items-center justify-center">
-                            <Card className="w-[350px]">
-                                <CardHeader>
-                                    {/* <CardTitle>Bedrijven</CardTitle> */}
-                                    <CardDescription className="text-center">
-                                        Kies een bedrijf om verder te gaan
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="grid w-full items-center gap-4">
-                                        <div className="flex flex-col space-y-4">
-                                            {loaderData.farmOptions.map(
-                                                (option) => (
-                                                    <div
-                                                        className="grid grid-cols-3 gap-x-3 items-center"
-                                                        key={option.b_id_farm}
-                                                    >
-                                                        <div className="col-span-2">
-                                                            <p className="text-sm font-medium leading-none">
+                        <div className="space-y-6 p-10 pb-0">
+                            <div className="flex-1 flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-4">
+                                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                                    {loaderData.farms.map((farm) => (
+                                        <Card key={farm.b_id_farm}>
+                                            <CardHeader>
+                                                <CardTitle>
+                                                    {farm.b_name_farm}
+                                                </CardTitle>
+                                                <CardDescription />
+                                            </CardHeader>
+                                            <CardContent>
+                                                <div className="grid gap-2">
+                                                    <div className="flex items-center">
+                                                        <p className="text-muted-foreground">
+                                                            Adres:
+                                                        </p>
+                                                        {farm.b_address_farm ? (
+                                                            <p className="font-medium ml-auto">
                                                                 {
-                                                                    option.b_name_farm
+                                                                    farm.b_address_farm
                                                                 }
                                                             </p>
-                                                            {/* <p className="text-sm text-muted-foreground">m@example.com</p> */}
-                                                        </div>
-
-                                                        <div className="">
-                                                            <Button
-                                                                asChild
-                                                                aria-label={`Selecteer ${option.b_name_farm}`}
-                                                            >
-                                                                <NavLink
-                                                                    to={`/farm/${option.b_id_farm}`}
-                                                                >
-                                                                    Selecteer
-                                                                </NavLink>
-                                                            </Button>
-                                                        </div>
+                                                        ) : (
+                                                            <p className="font-small text-muted-foreground ml-auto">
+                                                                {"Onbekend"}
+                                                            </p>
+                                                        )}
                                                     </div>
-                                                ),
-                                            )}
-                                        </div>
-                                    </div>
-                                </CardContent>
-                                <CardFooter className="flex flex-col items-center space-y-2">
+
+                                                    <div className="flex items-center">
+                                                        <p className="text-muted-foreground">
+                                                            Postcode:
+                                                        </p>
+                                                        {farm.b_postalcode_farm ? (
+                                                            <p className="font-medium ml-auto">
+                                                                {
+                                                                    farm.b_postalcode_farm
+                                                                }
+                                                            </p>
+                                                        ) : (
+                                                            <p className="font-small text-muted-foreground ml-auto">
+                                                                {"Onbekend"}
+                                                            </p>
+                                                        )}
+                                                    </div>
+                                                    {farm.b_businessid_farm && (
+                                                        <div className="flex items-center">
+                                                            <p className="text-muted-foreground">
+                                                                KvK-nummer:
+                                                            </p>
+                                                            <p className="font-medium ml-auto">
+                                                                {
+                                                                    farm.b_businessid_farm
+                                                                }
+                                                            </p>
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            </CardContent>
+                                            <CardFooter className="flex justify-end">
+                                                <Button
+                                                    asChild
+                                                    aria-label={`Selecteer ${farm.b_name_farm}`}
+                                                >
+                                                    <NavLink
+                                                        to={`/farm/${farm.b_id_farm}`}
+                                                    >
+                                                        Selecteer
+                                                    </NavLink>
+                                                </Button>
+                                            </CardFooter>
+                                        </Card>
+                                    ))}
+                                </div>
+
+                                <div className="flex flex-col items-center space-y-2 mt-8">
                                     <Separator />
                                     <p className="text-muted-foreground text-sm">
                                         Of maak een nieuw bedrijf aan:
                                     </p>
                                     <NavLink to="/farm/create">
-                                        <Button className="w-full">
+                                        <Button className="w-full md:w-auto">
                                             Nieuw bedrijf
                                         </Button>
                                     </NavLink>
-                                </CardFooter>
-                            </Card>
+                                </div>
+                            </div>
                         </div>
                     </>
                 )}
