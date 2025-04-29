5d97475f792efa6babca615372ca702fde6010e0
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-04-29 11:04:08 +0200

    Update fdm-core/src/principal.d.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Sven Verweij <37927107+SvenVw@users.noreply.github.com>

--- fdm-core/src/principal.d.ts
+++ fdm-core/src/principal.d.ts
@@ -1,8 +1,8 @@
 /**
  * @typedef Principal
- * @property {string} username - The username of slug of the principal.
+ * @property {string} username - The username or slug of the principal.
  * @property {string} initials - The initials of the principal.
- * @property {string | null} displayUsername - The display name of the principal (can be null).
+ * @property {string | null} displayUserName - The display name of the principal (can be null).
  * @property {string | null} image - The image URL of the principal (can be null).
  * @property {"user" | "organization"} type - The type of the principal (either "user" or "organization").
  * @property {boolean} isVerified - Indicates whether the principal is verified.
