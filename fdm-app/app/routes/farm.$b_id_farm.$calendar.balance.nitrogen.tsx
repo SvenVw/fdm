a0eab24c6ddd8b71c278c2e53912217207b0a04a
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-05-08 10:34:28 +0200

    feat: setup pages for nutrient balance app

---
+++ fdm-app/app/routes/farm.$b_id_farm.$calendar.balance.nitrogen.tsx
@@ -0,0 +1,125 @@
+import { getFarm, getFarms } from "@svenvw/fdm-core"
+import {
+    type LoaderFunctionArgs,
+    type MetaFunction,
+    Outlet,
+    data,
+    useLoaderData,
+} from "react-router"
+import { FarmContent } from "~/components/custom/farm/farm-content"
+import { FarmHeader } from "~/components/custom/farm/farm-header"
+import { FarmTitle } from "~/components/custom/farm/farm-title"
+import { SidebarInset } from "~/components/ui/sidebar"
+import { Toaster } from "~/components/ui/sonner"
+import { getSession } from "~/lib/auth.server"
+import { clientConfig } from "~/lib/config"
+import { handleLoaderError } from "~/lib/error"
+import { fdm } from "~/lib/fdm.server"
+import { useCalendarStore } from "~/store/calendar"
+
+// Meta
+export const meta: MetaFunction = () => {
+    return [
+        { title: `Stikstof | Nutriëntenbalans| ${clientConfig.name}` },
+        {
+            name: "description",
+            content: "Bekijk stikstof voor je nutriëntenbalans.",
+        },
+    ]
+}
+
+/**
+ * Loads farm details, farm options, and sidebar navigation items for a given farm.
+ *
+ * Retrieves the farm identifier from the route parameters, validates it, and uses the user's session from the request to
+ * fetch the corresponding farm details. It also retrieves all farms associated with the user, mapping them into simplified
+ * farm options. Additionally, it constructs sidebar page items for navigating to farm properties, access settings, and deletion.
+ *
+ * @param params - Route parameters; must include a valid `b_id_farm`.
+ * @returns An object containing the farm details, the farm identifier, an array of farm options, and an array of sidebar page items.
+ *
+ * @throws {Response} If `b_id_farm` is missing from the parameters.
+ * @throws {Response} If no farm matches the provided `b_id_farm`.
+ * @throws {Response} If no farms associated with the user are found.
+ */
+export async function loader({ request, params }: LoaderFunctionArgs) {
+    try {
+        // Get the farm id
+        const b_id_farm = params.b_id_farm
+        if (!b_id_farm) {
+            throw data("invalid: b_id_farm", {
+                status: 400,
+                statusText: "invalid: b_id_farm",
+            })
+        }
+
+        // Get the session
+        const session = await getSession(request)
+
+        // Get details of farm
+        const farm = await getFarm(fdm, session.principal_id, b_id_farm)
+        if (!farm) {
+            throw data("not found: b_id_farm", {
+                status: 404,
+                statusText: "not found: b_id_farm",
+            })
+        }
+
+        // Get a list of possible farms of the user
+        const farms = await getFarms(fdm, session.principal_id)
+        if (!farms || farms.length === 0) {
+            throw data("not found: farms", {
+                status: 404,
+                statusText: "not found: farms",
+            })
+        }
+
+        const farmOptions = farms.map((farm) => {
+            return {
+                b_id_farm: farm.b_id_farm,
+                b_name_farm: farm.b_name_farm,
+            }
+        })
+
+        // Return user information from loader
+        return {
+            farm: farm,
+            b_id_farm: b_id_farm,
+            farmOptions: farmOptions,
+        }
+    } catch (error) {
+        throw handleLoaderError(error)
+    }
+}
+
+/**
+ * Renders the layout for managing farm settings.
+ *
+ * This component displays a sidebar that includes the farm header, navigation options, and a link to farm fields.
+ * It also renders a main section containing the farm title, description, nested routes via an Outlet, and a notification toaster.
+ */
+export default function FarmBalanceNitrogenBlock() {
+    const loaderData = useLoaderData<typeof loader>()
+    const calendar = useCalendarStore((state) => state.calendar)
+
+    return (
+        <SidebarInset>
+            <FarmHeader
+                farmOptions={loaderData.farmOptions}
+                b_id_farm={loaderData.b_id_farm}
+                // action={{
+                //     to: `/farm/${loaderData.b_id_farm}/${calendar}/field`,
+                //     label: "Naar percelen",
+                // }}
+            />
+            <main>
+                <FarmTitle
+                    title={"Stikstof"}
+                    description={"Bekijk stikstof voor je nutriëntenbalans."}
+                />
+                <Outlet />
+                <Toaster />
+            </main>
+        </SidebarInset>
+    )
+}
