6934c2c4f27847223db3777abbef2db765bd0967
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-05-08 16:17:24 +0200

    feat: export the types of NitrogenBalance and NitrogenBalanceInput

--- fdm-calculator/src/index.ts
+++ fdm-calculator/src/index.ts
@@ -3,3 +3,7 @@ export { getDoseForField } from "./doses/get-dose-field"
 export { collectInputForNitrogenBalance } from "./balance/nitrogen/input"
 export { calculateNitrogenBalance } from "./balance/nitrogen/index"
 export type { Dose } from "./doses/d"
+export type {
+    NitrogenBalanceInput,
+    NitrogenBalance,
+} from "./balance/nitrogen/types"
