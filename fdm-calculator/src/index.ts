642192f76f527a0c857490f220186abf85ad485b
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-05-08 13:02:20 +0200

    feat: eable export of collectInputForNitrogenBalance and calculateNitrogenBalance

--- fdm-calculator/src/index.ts
+++ fdm-calculator/src/index.ts
@@ -1,3 +1,5 @@
 export { calculateDose } from "./doses/calculate-dose"
 export { getDoseForField } from "./doses/get-dose-field"
+export { collectInputForNitrogenBalance } from "./balance/nitrogen/input"
+export { calculateNitrogenBalance } from "./balance/nitrogen/index"
 export type { Dose } from "./doses/d"
