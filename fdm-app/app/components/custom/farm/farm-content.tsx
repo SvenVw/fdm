3a7707d2f3b68d243c3dc1eda580bd9d778443c4
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-06-03 10:01:49 +0200

    fix: layout issue

--- fdm-app/app/components/custom/farm/farm-content.tsx
+++ fdm-app/app/components/custom/farm/farm-content.tsx
@@ -12,10 +12,10 @@ interface FarmContentProps {
 
 export function FarmContent({ sidebarItems, children }: FarmContentProps) {
     return (
-        <div className="space-y-6 p-10 pb-0">
-            <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <div className="space-y-6 px-10 pb-0">
+            <div className="flex flex-col space-y-0 lg:flex-row lg:space-x-8 lg:space-y-0">
                 {sidebarItems && (
-                    <aside className="-mx-4 lg:w-1/5">
+                    <aside className="lg:w-1/5">
                         <SidebarPage items={sidebarItems} />
                     </aside>
                 )}
