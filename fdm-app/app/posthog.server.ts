d52a15b0f50601d0725b1a1ea6813d8c7f4462c8
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-06-19 17:11:44 +0200

    fix: envirnomental naming and run on server side

--- fdm-app/app/posthog.server.ts
+++ fdm-app/app/posthog.server.ts
@@ -4,8 +4,8 @@ let posthogClient: PostHog | null = null
 
 export default function PostHogClient(): PostHog | null {
     if (!posthogClient) {
-        const posthogHost = process.env.VITE_PUBLIC_POSTHOG_HOST
-        const posthogKey = process.env.VITE_PUBLIC_POSTHOG_KEY
+        const posthogHost = process.env.PUBLIC_POSTHOG_HOST
+        const posthogKey = process.env.PUBLIC_POSTHOG_KEY
         if (posthogHost && posthogKey?.startsWith("phc")) {
             posthogClient = new PostHog(posthogKey, {
                 host: posthogHost,
