7be3c2cb5ffee264090dc1918ea56103afd95e09
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-06-19 11:25:30 +0200

    feat: add dropdown month and year selection and text input

--- fdm-app/app/components/custom/date-picker.tsx
+++ fdm-app/app/components/custom/date-picker.tsx
@@ -1,5 +1,13 @@
-import { cn } from "@/app/lib/utils"
+import * as React from "react"
+import { CalendarIcon } from "lucide-react"
 import { Button } from "~/components/ui/button"
+import { Calendar } from "~/components/ui/calendar"
+import { Input } from "~/components/ui/input"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "~/components/ui/popover"
 import {
     FormControl,
     FormDescription,
@@ -8,11 +16,23 @@ import {
     FormLabel,
     FormMessage,
 } from "~/components/ui/form"
-import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover"
 import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { Calendar } from "~/components/ui/calendar"
 import { nl } from "date-fns/locale"
+
+function formatDate(date: Date | undefined) {
+    if (!date) {
+        return ""
+    }
+
+    return format(date, "d MMMM yyyy", { locale: nl })
+}
+
+function isValidDate(date: Date | undefined) {
+    if (!date) {
+        return false
+    }
+    return !Number.isNaN(date.getTime())
+}
 
 interface DatePickerProps {
     form: any
@@ -27,46 +47,94 @@ export function DatePicker({
     label,
     description,
 }: DatePickerProps) {
+    const [open, setOpen] = React.useState(false)
+    const [date, setDate] = React.useState<Date | undefined>(
+        form.getValues(name),
+    )
+    const [month, setMonth] = React.useState<Date | undefined>(date)
+    const [value, setValue] = React.useState(formatDate(date))
+
+    React.useEffect(() => {
+        const formDate = form.getValues(name)
+        if (formDate && formDate.getTime() !== date?.getTime()) {
+            setDate(formDate)
+            setMonth(formDate)
+            setValue(formatDate(formDate))
+        }
+    }, [form, name, date])
+
     return (
         <FormField
             control={form.control}
             name={name}
             render={({ field }) => (
-                <FormItem className="flex flex-col gap-3">
+                <FormItem className="flex flex-col gap-3 w-[240px]">
                     <FormLabel>{label}</FormLabel>
-                    <Popover>
-                        <PopoverTrigger asChild>
-                            <FormControl>
-                                <Button
-                                    variant={"outline"}
-                                    className={cn(
-                                        "w-[240px] justify-between font-normal",
-                                        !field.value && "text-muted-foreground",
-                                    )}
-                                >
-                                    {field.value ? (
-                                        format(field.value, "d MMM yyyy")
-                                    ) : (
-                                        <span>Kies een datum</span>
-                                    )}
-                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                </Button>
-                            </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
-                            <Calendar
-                                mode="single"
-                                selected={field.value}
-                                onSelect={field.onChange}
-                                disabled={(date) =>
-                                    date < new Date("1970-01-01")
-                                }
-                                locale={nl}
-                                className="rounded-lg border shadow-sm"
-                                initialFocus
+                    <div className="relative flex gap-2">
+                        <FormControl>
+                            <Input
+                                id={field.name}
+                                value={value}
+                                placeholder="Kies een datum"
+                                className="bg-background pr-10"
+                                onChange={(e) => {
+                                    const newDate = new Date(e.target.value)
+                                    setValue(e.target.value)
+                                    if (isValidDate(newDate)) {
+                                        setDate(newDate)
+                                        setMonth(newDate)
+                                        field.onChange(newDate)
+                                    } else {
+                                        field.onChange(undefined)
+                                    }
+                                }}
+                                onKeyDown={(e) => {
+                                    if (e.key === "ArrowDown") {
+                                        e.preventDefault()
+                                        setOpen(true)
+                                    }
+                                }}
                             />
-                        </PopoverContent>
-                    </Popover>
+                        </FormControl>
+                        <Popover open={open} onOpenChange={setOpen}>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    id={`${field.name}-picker`}
+                                    variant="ghost"
+                                    className="absolute top-1/2 right-2 size-6 -translate-y-1/2"
+                                >
+                                    <CalendarIcon className="size-3.5" />
+                                    <span className="sr-only">Select date</span>
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent
+                                className="w-auto overflow-hidden p-0"
+                                align="end"
+                                alignOffset={-8}
+                                sideOffset={10}
+                            >
+                                <Calendar
+                                    mode="single"
+                                    selected={date}
+                                    captionLayout="dropdown"
+                                    month={month}
+                                    onMonthChange={setMonth}
+                                    onSelect={(selectedDate) => {
+                                        setDate(selectedDate)
+                                        setValue(formatDate(selectedDate))
+                                        field.onChange(selectedDate)
+                                        setOpen(false)
+                                    }}
+                                    disabled={(date) =>
+                                        date < new Date("1970-01-01")
+                                    }
+                                    locale={nl}
+                                    className="rounded-lg border shadow-sm"
+                                    initialFocus
+                                />
+                            </PopoverContent>
+                        </Popover>
+                    </div>
                     <FormDescription>{description}</FormDescription>
                     <FormMessage />
                 </FormItem>
