2fb7656f72f31ca6648d8838f99017eab8f7e608
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-06-30 11:45:47 +0200

    refactor: some improvements

--- fdm-calculator/src/norms/index.ts
+++ fdm-calculator/src/norms/index.ts
@@ -3,8 +3,7 @@ import { getNL2025FosfaatGebruiksNorm } from "./nl/2025/fosfaatgebruiksnorm"
 import { getNL2025StikstofGebruiksNorm } from "./nl/2025/stikstofgebruiksnorm"
 
 export function createFunctionsForNorms(b_region: string, year: number) {
-
-    if (b_region == 'NL') {
+    if (b_region === "NL") {
         if (year === 2025) {
             return {
                 calculateNormForNitrogen: getNL2025StikstofGebruiksNorm,
@@ -14,30 +13,30 @@ export function createFunctionsForNorms(b_region: string, year: number) {
                 calculateNormForPhosphate: getNL2025FosfaatGebruiksNorm,
                 collectInputForNormForPhosphate: undefined,
             }
-        } else  {
-            throw new Error('Year not supported')
         }
-    } else {
-        throw new Error('Region not supported')
+        throw new Error("Year not supported")
     }
+    throw new Error("Region not supported")
 }
 
-export function createFunctionsForFertilizerApplicationFilling(b_region: string, year: number) {
-
-    if (b_region == 'NL') {
+export function createFunctionsForFertilizerApplicationFilling(
+    b_region: string,
+    year: number,
+) {
+    if (b_region === "NL") {
         if (year === 2025) {
             return {
                 calculateFertilizerApplicationFillingForNitrogen: undefined,
-                collectInputForFertilizerApplicationFillingForNitrogen: undefined,
+                collectInputForFertilizerApplicationFillingForNitrogen:
+                    undefined,
                 calculateFertilizerApplicationFillingForManure: undefined,
                 collectInputForFertilizerApplicationFillingForManure: undefined,
                 calculateFertilizerApplicationFillingForPhosphate: undefined,
-                collectInputForFertilizerApplicationFillingForPhosphate: undefined,
+                collectInputForFertilizerApplicationFillingForPhosphate:
+                    undefined,
             }
-        } else  {
-            throw new Error('Year not supported')
         }
-    } else {
-        throw new Error('Region not supported')
+        throw new Error("Year not supported")
     }
+    throw new Error("Region not supported")
 }
