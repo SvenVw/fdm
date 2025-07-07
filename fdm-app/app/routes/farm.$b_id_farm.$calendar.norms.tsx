74974d8b58185161de7e7ccd4f6001b43a93d3f1
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-07-07 17:45:50 +0200

    feat: add placeholder for at norms for years that are not possible yet

--- fdm-app/app/routes/farm.$b_id_farm.$calendar.norms.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.$calendar.norms.tsx
@@ -4,6 +4,7 @@ import {
     data,
     type LoaderFunctionArgs,
     type MetaFunction,
+    NavLink,
     useLoaderData,
 } from "react-router"
 import { FarmTitle } from "~/components/blocks/farm/farm-title"
@@ -13,13 +14,14 @@ import { FarmNorms } from "~/components/blocks/norms/farm-norms"
 import { FieldNorms } from "~/components/blocks/norms/field-norms"
 import { SidebarInset } from "~/components/ui/sidebar"
 import { getSession } from "~/lib/auth.server"
-import { getTimeframe } from "~/lib/calendar"
+import { getCalendar, getTimeframe } from "~/lib/calendar"
 import { clientConfig } from "~/lib/config"
 import { handleLoaderError } from "~/lib/error"
 import { fdm } from "~/lib/fdm.server"
 import { Separator } from "../components/ui/separator"
 import { Alert, AlertDescription } from "../components/ui/alert"
 import { AlertTriangle } from "lucide-react"
+import { Button } from "../components/ui/button"
 
 // Meta
 export const meta: MetaFunction = () => {
@@ -51,6 +53,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
         // Get timeframe from calendar store
         const timeframe = getTimeframe(params)
+        const calendar = getCalendar(params)
 
         // Get details of farm
         const farm = await getFarm(fdm, session.principal_id, b_id_farm)
@@ -95,6 +98,19 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
             }
         })
 
+        if (calendar !== "2025") {
+            return {
+                farm: farm,
+                b_id_farm: b_id_farm,
+                b_id: b_id,
+                calendar: calendar,
+                farmOptions: farmOptions,
+                fieldOptions: fieldOptions,
+                fieldNorms: undefined,
+                farmNorms: undefined,
+            }
+        }
+
         // Calculate norms per field
         const functionsForms = createFunctionsForNorms("NL", 2025)
 
@@ -137,6 +153,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
             farm: farm,
             b_id_farm: b_id_farm,
             b_id: b_id,
+            calendar: calendar,
             farmOptions: farmOptions,
             fieldOptions: fieldOptions,
             fieldNorms: fieldNorms,
@@ -166,27 +183,54 @@ export default function FarmNormsBlock() {
                     }
                 />
                 {/* Disclaimer */}
-                <div className="px-10">
-                    <Alert className="mb-8  border-amber-200 bg-amber-50">
-                        <AlertTriangle className="h-4 w-4 text-amber-600" />
-                        <AlertDescription className="text-amber-800">
-                            <strong>Disclaimer:</strong> Deze getallen zijn
-                            uitsluitend bedoeld voor informatieve doeleinden. De
-                            getoonde gebruiksnormen zijn indicatief en dienen te
-                            worden geverifieerd voor juridische naleving.
-                            Raadpleeg altijd de officiële RVO-publicaties en uw
-                            adviseur voor definitieve normen.
-                        </AlertDescription>
-                    </Alert>
-                </div>
+                {!loaderData.farmNorms && loaderData.fieldNorms ? (
+                    <div className="px-10">
+                        <Alert className="mb-8  border-amber-200 bg-amber-50">
+                            <AlertTriangle className="h-4 w-4 text-amber-600" />
+                            <AlertDescription className="text-amber-800">
+                                <strong>Disclaimer:</strong> Deze getallen zijn
+                                uitsluitend bedoeld voor informatieve
+                                doeleinden. De getoonde gebruiksnormen zijn
+                                indicatief en dienen te worden geverifieerd voor
+                                juridische naleving. Raadpleeg altijd de
+                                officiële RVO-publicaties en uw adviseur voor
+                                definitieve normen.
+                            </AlertDescription>
+                        </Alert>
+                    </div>
+                ) : null}
 
                 <div className="space-y-6 px-10 pb-16">
-                    <FarmNorms farmNorms={loaderData.farmNorms} />
-                    <Separator className="my-8" />
-                    <FieldNorms
-                        fieldNorms={loaderData.fieldNorms}
-                        fieldOptions={loaderData.fieldOptions}
-                    />
+                    {loaderData.farmNorms && loaderData.fieldNorms ? (
+                        <>
+                            <FarmNorms farmNorms={loaderData.farmNorms} />
+                            <Separator className="my-8" />
+                            <FieldNorms
+                                fieldNorms={loaderData.fieldNorms}
+                                fieldOptions={loaderData.fieldOptions}
+                            />
+                        </>
+                    ) : (
+                        <div className="mx-auto flex h-full w-full items-center flex-col justify-center space-y-6">
+                            <div className="flex flex-col space-y-2 text-center">
+                                <h1 className="text-2xl font-semibold tracking-tight">
+                                    Helaas, nog geen gebruiksnormen beschikbaar
+                                    voor {loaderData.calendar}
+                                </h1>
+                                <p className="text-sm text-muted-foreground">
+                                    Op dit moment kunnen we alleen nog de
+                                    gebruiksnormen voor 2025 berekenen en
+                                    weergeven.
+                                </p>
+                                <NavLink
+                                    to={`/farm/${loaderData.b_id_farm}/2025/norms`}
+                                    asChild
+                                >
+                                    <Button>Ga naar 2025</Button>
+                                </NavLink>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </main>
         </SidebarInset>
