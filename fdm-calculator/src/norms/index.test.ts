4e141c7f633b808126651b8c88759c76195ec21a
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-07-22 08:52:20 +0200

    test: fix

--- fdm-calculator/src/norms/index.test.ts
+++ fdm-calculator/src/norms/index.test.ts
@@ -1,5 +1,8 @@
 import { describe, it, expect } from "vitest"
-import { createFunctionsForNorms, createFunctionsForFertilizerApplicationFilling } from "./index"
+import {
+    createFunctionsForNorms,
+    createFunctionsForFertilizerApplicationFilling,
+} from "./index"
 import { collectNL2025InputForNorms } from "./nl/2025/input"
 import { getNL2025StikstofGebruiksNorm } from "./nl/2025/stikstofgebruiksnorm"
 import { getNL2025DierlijkeMestGebruiksNorm } from "./nl/2025/dierlijke-mest-gebruiksnorm"
@@ -10,35 +13,70 @@ describe("createFunctionsForNorms", () => {
     it("should return the correct functions for NL region and year 2025", () => {
         const functions = createFunctionsForNorms("NL", "2025")
         expect(functions.collectInputForNorms).toBe(collectNL2025InputForNorms)
-        expect(functions.calculateNormForNitrogen).toBe(getNL2025StikstofGebruiksNorm)
-        expect(functions.calculateNormForManure).toBe(getNL2025DierlijkeMestGebruiksNorm)
-        expect(functions.calculateNormForPhosphate).toBe(getNL2025FosfaatGebruiksNorm)
-        expect(functions.aggregateNormsToFarmLevel).toBe(aggregateNormsToFarmLevel)
+        expect(functions.calculateNormForNitrogen).toBe(
+            getNL2025StikstofGebruiksNorm,
+        )
+        expect(functions.calculateNormForManure).toBe(
+            getNL2025DierlijkeMestGebruiksNorm,
+        )
+        expect(functions.calculateNormForPhosphate).toBe(
+            getNL2025FosfaatGebruiksNorm,
+        )
+        expect(functions.aggregateNormsToFarmLevel).toBe(
+            aggregateNormsToFarmLevel,
+        )
     })
 
     it("should throw an error for an unsupported year", () => {
-        expect(() => createFunctionsForNorms("NL"," 2024")).toThrow("Year not supported")
+        expect(() => createFunctionsForNorms("NL", " 2024")).toThrow(
+            "Year not supported",
+        )
     })
 
     it("should throw an error for an unsupported region", () => {
-        expect(() => createFunctionsForNorms("BE", "2025")).toThrow("Region not supported")
+        expect(() => createFunctionsForNorms("BE", "2025")).toThrow(
+            "Region not supported",
+        )
     })
 })
 
 describe("createFunctionsForFertilizerApplicationFilling", () => {
     it("should return the correct functions for NL region and year 2025", () => {
-        const functions = createFunctionsForFertilizerApplicationFilling("NL", "2025")
-        expect(functions.collectInputForFertilizerApplicationFilling).toBeUndefined()
-        expect(functions.calculateFertilizerApplicationFillingForNitrogen).toBeUndefined()
-        expect(functions.calculateFertilizerApplicationFillingForManure).toBeUndefined()
-        expect(functions.calculateFertilizerApplicationFillingForPhosphate).toBeUndefined()
+        const functions = createFunctionsForFertilizerApplicationFilling(
+            "NL",
+            "2025",
+        )
+        expect(
+            functions.collectInputForFertilizerApplicationFilling,
+        ).toThrowError(
+            "collectInputForFertilizerApplicationFilling is not implemented yet",
+        )
+        expect(
+            functions.calculateFertilizerApplicationFillingForNitrogen,
+        ).toThrowError(
+            "calculateFertilizerApplicationFillingForNitrogen is not implemented yet",
+        )
+        expect(
+            functions.calculateFertilizerApplicationFillingForManure,
+        ).toThrowError(
+            "calculateFertilizerApplicationFillingForManure is not implemented yet",
+        )
+        expect(
+            functions.calculateFertilizerApplicationFillingForPhosphate,
+        ).toThrowError(
+            "calculateFertilizerApplicationFillingForPhosphate is not implemented yet",
+        )
     })
 
     it("should throw an error for an unsupported year", () => {
-        expect(() => createFunctionsForFertilizerApplicationFilling("NL", "2024")).toThrow("Year not supported")
+        expect(() =>
+            createFunctionsForFertilizerApplicationFilling("NL", "2024"),
+        ).toThrow("Year not supported")
     })
 
     it("should throw an error for an unsupported region", () => {
-        expect(() => createFunctionsForFertilizerApplicationFilling("BE", "2025")).toThrow("Region not supported")
+        expect(() =>
+            createFunctionsForFertilizerApplicationFilling("BE", "2025"),
+        ).toThrow("Region not supported")
     })
 })
