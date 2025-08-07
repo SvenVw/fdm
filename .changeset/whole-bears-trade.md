d0b583eddabd8619805d1078bfa40debedb5ccf7
Mert Bora İnevi <bora.inevi@nmi-agro.nl>
2025-08-07 17:26:33 +0200

    Selected fields have an outline instead of a different fill color

---
+++ .changeset/whole-bears-trade.md
@@ -0,0 +1,5 @@
+---
+"@svenvw/fdm-app": minor
+---
+
+Selected fields now have an outline instead of a different fill color in the farm creation wizard atlas.
--- fdm-app/app/components/blocks/atlas/atlas-styles.tsx
+++ fdm-app/app/components/blocks/atlas/atlas-styles.tsx
@@ -1,68 +1,65 @@
-import type {
-    FillLayerSpecification,
-    LayerProps,
-    SymbolLayerSpecification,
-} from "react-map-gl/mapbox"
+import type { ExpressionSpecification } from "mapbox-gl"
+import type { LayerProps } from "react-map-gl/mapbox"
+import {
+    getCultivationColor,
+    getCultivationTypesHavingColors,
+} from "~/components/custom/cultivation-colors"
 
-export const CROP_ROTATION_COLORS = {
-    grass: "#558B2F",
-    maize: "#FBC02D",
-    cereal: "#C2B280",
-    potato: "#8D6E63",
-    sugarbeet: "#9B2D30",
-    rapeseed: "#D4AC0D",
-    clover: "#8BC34A",
-    alfalfa: "#7E57C2",
-    catchcrop: "#4DD0E1",
-    nature: "#00796B",
-    starch: "#F57C00",
-    other: "#9E9E9E",
-}
+const baseFieldsFillColorExpr: ExpressionSpecification = [
+    "match",
+    ["get", "b_lu_croprotation"],
+]
 
-const baseFieldsFillColorExpr = ["match", ["get", "b_lu_croprotation"]]
-
-for (const [key, value] of Object.entries(CROP_ROTATION_COLORS)) {
-    baseFieldsFillColorExpr.push(key, value)
+for (const key of getCultivationTypesHavingColors()) {
+    baseFieldsFillColorExpr.push(key, getCultivationColor(key))
 }
 
 baseFieldsFillColorExpr.push("#60a5fa")
 
-export function getFieldsStyle(layerId: string): LayerProps &
-    (
-        | {
-              id: string
-              type: "fill"
-              paint: FillLayerSpecification["paint"]
-          }
-        | {
-              id: string
-              type: "symbol"
-              layout: SymbolLayerSpecification["layout"]
-              paint: SymbolLayerSpecification["paint"]
-          }
-    ) {
-    const baseFieldsStyle: Omit<FillLayerSpecification, "id" | "source"> = {
-        type: "fill",
-        paint: {
-            "fill-color": baseFieldsFillColorExpr,
-            "fill-opacity": 0.5,
-            "fill-outline-color": "#1e3a8a",
-        },
-    } as FillLayerSpecification // Cast to FillLayerSpecification to allow modification of paint
+export function getFieldsStyle(layerId: string) {
+    const style = getFieldsStyleInner(layerId) as LayerProps
+    style.id = layerId
+    return style
+}
 
-    const fieldsStyle = {
-        ...baseFieldsStyle,
-        id: layerId,
-    } as LayerProps & FillLayerSpecification // Cast to FillLayerSpecification to allow modification of paint
+function getFieldsStyleInner(layerId: string): Omit<LayerProps, "id"> {
+    const baseFillStyles = {
+        "fill-outline-color": "#1e3a8a",
+    }
+
+    const baseLineStyles = {
+        "line-width": 4,
+    }
 
     if (layerId === "fieldsSelected") {
-        fieldsStyle.paint["fill-color"] = "#f43f5e"
-        fieldsStyle.paint["fill-opacity"] = 0.8
-    }
-    if (layerId === "fieldsSaved") {
-        fieldsStyle.paint["fill-color"] = "#10b981"
-        fieldsStyle.paint["fill-opacity"] = 0.8
+        return {
+            type: "line",
+            paint: {
+                ...baseLineStyles,
+                "line-color": "#ffcf0d",
+            },
+        }
     }
 
-    return fieldsStyle
+    if (layerId === "fieldsSaved") {
+        return {
+            type: "fill",
+            paint: {
+                ...baseFillStyles,
+                "fill-color": "#10b981",
+                "fill-opacity": 0.8,
+            },
+        }
+    }
+
+    // default styles
+    return {
+        type: "fill",
+        paint: {
+            ...baseFillStyles,
+            "fill-color": baseFieldsFillColorExpr,
+            "fill-opacity": 0.8,
+            "fill-outline-color": "#1e3a8a",
+        },
+    }
 }
