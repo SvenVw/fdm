8c45b373dbbec656e0265af943c5825f96a34b7d
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-08-12 12:36:22 +0200

    fix: upload sourcemap when sentry auth token is provided

--- fdm-app/react-router.config.ts
+++ fdm-app/react-router.config.ts
@@ -1,4 +1,14 @@
 import type { Config } from "@react-router/dev/config"
+import { sentryOnBuildEnd } from "@sentry/react-router"
 export default {
     ssr: true,
+    buildEnd: async ({ viteConfig, reactRouterConfig, buildManifest }) => {
+        if (process.env.SENTRY_AUTH_TOKEN !== undefined) {
+            await sentryOnBuildEnd({
+                viteConfig,
+                reactRouterConfig,
+                buildManifest,
+            })
+        }
+    },
 } satisfies Config
