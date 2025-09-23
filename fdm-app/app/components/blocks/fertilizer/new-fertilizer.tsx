3cd3395ed2ae90c1d90aac84076d9c05274ae21c
Mert Bora İnevi <bora.inevi@nmi-agro.nl>
2025-09-23 10:38:21 +0200

    Move new fertilizer index code back into routes

---
+++ fdm-app/app/components/blocks/fertilizer/new-fertilizer.tsx
@@ -0,0 +1,46 @@
+import { Link, NavLink } from "react-router"
+import { Card, CardContent } from "../../ui/card"
+import { Fertilizer } from "./columns"
+
+export function CustomFertilizerButton() {
+    return (
+        <Link to="custom" className="block">
+            <Card>
+                <CardContent className="p-4">
+                    <h2 className="text-xl font-semibold">
+                        Maak een nieuwe meststof aan
+                    </h2>
+                    <p className="text-muted-foreground">
+                        Begin met een leeg formulier
+                    </p>
+                </CardContent>
+            </Card>
+        </Link>
+    )
+}
+
+export function BasedOffFertilizerButton({
+    fertilizer,
+}: {
+    fertilizer: Fertilizer
+}) {
+    return (
+        <NavLink
+            key={fertilizer.p_id}
+            to={`${fertilizer.p_id}`}
+            className="block"
+        >
+            <Card>
+                <CardContent className="p-4">
+                    <h3 className="text-lg font-semibold">
+                        {fertilizer.p_name_nl || "Onbekend"}
+                    </h3>
+                    {/* <p className="text-muted-foreground">
+                                    {fertilizer.p_description ||
+                                        "No description available."}
+                                </p> */}
+                </CardContent>
+            </Card>
+        </NavLink>
+    )
+}
--- fdm-app/app/routes/farm.$b_id_farm.fertilizers.new._index.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.fertilizers.new._index.tsx
@@ -1,9 +1,13 @@
 import { getFertilizers } from "@svenvw/fdm-core"
 import type { LoaderFunctionArgs } from "react-router"
 import { useLoaderData } from "react-router"
-import { NewFertilizerIndex } from "@/app/components/blocks/fertilizer/new-index-page"
 import { getSession } from "~/lib/auth.server"
 import { fdm } from "~/lib/fdm.server"
+import type { Fertilizer } from "~/components/blocks/fertilizer/columns"
+import {
+    BasedOffFertilizerButton,
+    CustomFertilizerButton,
+} from "../components/blocks/fertilizer/new-fertilizer"
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
     const { b_id_farm } = params
@@ -22,8 +26,27 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     return { b_id_farm: b_id_farm, fertilizers: fertilizers }
 }
 
+/**
+ * Renders the new fertilizer wizard start page
+ *
+ * This component includes a button that can be used to fill everything from scratch.
+ * Below that it includes a button for each existing fertilizer which the user can click to fill in the new fertilizer form values on the next page based on the corresponding fertilizer.
+ */
 export default function NewFertilizerIndexPage() {
-    const loaderData = useLoaderData<typeof loader>()
+    const { fertilizers } = useLoaderData()
 
-    return <NewFertilizerIndex loaderData={loaderData} />
+    return (
+        <div className="space-y-4">
+            <CustomFertilizerButton />
+            <h2 className="text-xl font-bold">Of baseer op een meststof</h2>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                {fertilizers.map((fertilizer: Fertilizer) => (
+                    <BasedOffFertilizerButton
+                        key={fertilizer.p_id}
+                        fertilizer={fertilizer}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
