81497ddd66fa0ac919bab724365b91dad257dcc3
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-10-15 16:11:08 +0200

    feat: add table 11

---
+++ fdm-calculator/src/norms/nl/2025/filling/types.d.ts
@@ -0,0 +1,8 @@
+import type { fdmSchema } from "@svenvw/fdm-core"
+
+export type Table11Mestcodes = {
+    p_type_rvo: fdmSchema.fertilizersCatalogueTypeSelect["p_type_rvo"]
+    p_type_animal: boolean
+    p_n_rt?: number
+    p_p_rt?: number
+}[]
