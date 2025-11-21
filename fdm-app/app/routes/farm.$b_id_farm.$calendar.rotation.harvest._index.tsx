3ea2fc20aa5f6b9f4ff0bde58aa950c8909d5dc2
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-11-21 11:30:39 +0100

    refactor: improve error message for missing harvest parameters

--- fdm-app/app/routes/farm.$b_id_farm.$calendar.rotation.harvest._index.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.$calendar.rotation.harvest._index.tsx
@@ -72,6 +72,7 @@ import { handleActionError, handleLoaderError } from "~/lib/error"
 import { fdm } from "~/lib/fdm.server"
 import { extractFormValuesFromRequest } from "~/lib/form"
 import { modifySearchParams } from "~/lib/url-utils"
+import { getHarvestParameterLabel } from "../components/blocks/harvest/parameters"
 
 export const meta: MetaFunction = () => {
     return [
@@ -879,6 +880,9 @@ export async function action({ request, params }: ActionFunctionArgs) {
                     missingParameters.push(param)
                 }
             }
+            const missingParameterLabels = missingParameters.map((param) => {
+                return getHarvestParameterLabel(param)
+            })
 
             if (missingParameters.length > 0) {
                 return dataWithWarning(
@@ -887,7 +891,7 @@ export async function action({ request, params }: ActionFunctionArgs) {
                             ", ",
                         )}`,
                     },
-                    `Missing required harvest parameters: ${missingParameters.join(
+                    `Voor de volgende parameters ontbreekt een waarde: ${missingParameterLabels.join(
                         ", ",
                     )}`,
                 )
