b4ff19a1aa557ae12ecefc3bf94d48412a57742f
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-12-04 17:07:12 +0100

    feat: Enable to create a farm for next year

--- fdm-app/app/routes/farm.create._index.tsx
+++ fdm-app/app/routes/farm.create._index.tsx
@@ -51,6 +51,7 @@ import { clientConfig } from "~/lib/config"
 import { handleActionError } from "~/lib/error"
 import { fdm } from "~/lib/fdm.server"
 import { extractFormValuesFromRequest } from "~/lib/form"
+import { getCalendarSelection } from "../lib/calendar"
 
 // Meta
 export const meta: MetaFunction = () => {
@@ -89,9 +90,11 @@ const FormSchema = z.object({
 
 // Loader
 export async function loader({ request }: LoaderFunctionArgs) {
+    const yearSelection = getCalendarSelection()
+
     return {
-        b_name_farm: null,
         year: new Date().getFullYear(),
+        yearSelection: yearSelection,
     }
 }
 
@@ -101,24 +104,18 @@ export async function loader({ request }: LoaderFunctionArgs) {
  * @returns The JSX element representing the add farm page.
  */
 export default function AddFarmPage() {
-    const loaderData = useLoaderData<typeof loader>()
+    const { year, yearSelection } = useLoaderData()
 
     const form = useRemixForm<z.infer<typeof FormSchema>>({
         mode: "onTouched",
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            b_name_farm: loaderData.b_name_farm ?? "",
-            year: loaderData.year,
+            b_name_farm: "",
+            year: year,
             has_derogation: false,
-            derogation_start_year: loaderData.year,
+            derogation_start_year: 2025,
         },
     })
-
-    const currentYear = new Date().getFullYear()
-    const years = Array.from(
-        { length: currentYear - 2020 + 1 },
-        (_, i) => currentYear - i,
-    )
 
     return (
         <SidebarInset>
@@ -289,9 +286,9 @@ export default function AddFarmPage() {
                                                                     </SelectTrigger>
                                                                 </FormControl>
                                                                 <SelectContent>
-                                                                    {years.map(
+                                                                    {yearSelection.map(
                                                                         (
-                                                                            year,
+                                                                            year: number,
                                                                         ) => (
                                                                             <SelectItem
                                                                                 key={
