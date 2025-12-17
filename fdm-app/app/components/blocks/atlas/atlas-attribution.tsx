e6b60333b6373eca73bd86f69f6cf3c16d4e1e83
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-12-17 15:34:46 +0100

    refactor: some improvements to attribution

--- fdm-app/app/components/blocks/atlas/atlas-attribution.tsx
+++ fdm-app/app/components/blocks/atlas/atlas-attribution.tsx
@@ -9,10 +9,13 @@ export function MapTilerAttribution() {
         <a
             href="https://www.maptiler.com"
             style={{ position: "absolute", left: 10, bottom: 10, zIndex: 999 }}
+             rel="noopener noreferrer"
+             target="_blank"
         >
             <img
                 src="https://api.maptiler.com/resources/logo.svg"
                 alt="MapTiler logo"
+                onError={(e) => { e.currentTarget.style.display = 'none' }}
             />
         </a>
     )
