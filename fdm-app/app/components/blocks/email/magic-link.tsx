2a71ddc813729b227140edb63cc81fed1470d47d
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2025-12-18 15:55:56 +0100

    chore: update react-email packages

--- fdm-app/app/components/blocks/email/magic-link.tsx
+++ fdm-app/app/components/blocks/email/magic-link.tsx
@@ -9,9 +9,9 @@ import {
     Link,
     Preview,
     Section,
-    Tailwind,
     Text,
 } from "@react-email/components"
+import { Tailwind } from "@react-email/tailwind"
 
 interface MagicLinkEmailProps {
     url: string
@@ -44,8 +44,8 @@ export const MagicLinkEmail = ({
         <Preview>{`Link om aan te melden bij ${appName}`}</Preview>
         <Tailwind>
             <Body className="bg-white my-auto mx-auto font-sans">
-                <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
-                    <Section className="mt-[30px] text-center">
+                <Container className="border border-solid border-[#eaeaea] rounded my-10 mx-auto p-5 w-116.25">
+                    <Section className="mt-7.5 text-center">
                         <Img
                             src={`${appBaseUrl}/fdm-high-resolution-logo-transparent.png`}
                             width="150"
@@ -53,54 +53,54 @@ export const MagicLinkEmail = ({
                             className="my-0 mx-auto"
                         />
                     </Section>
-                    <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+                    <Heading className="text-black text-[24px] font-normal text-center p-0 my-7.5 mx-0">
                         Aanmelden bij {appName}
                     </Heading>
-                    <Text className="text-black text-[14px] leading-[24px]">
+                    <Text className="text-black text-[14px] leading-6">
                         Hallo,
                     </Text>
-                    <Text className="text-black text-[14px] leading-[24px]">
+                    <Text className="text-black text-[14px] leading-6">
                         U heeft een link aangevraagd om in te loggen bij{" "}
                         {appName}. Klik op de knop hieronder om verder te gaan:
                     </Text>
-                    <Section className="mt-[32px] mb-[32px] text-center">
+                    <Section className="mt-8 mb-8 text-center">
                         <Button
                             href={url}
                             aria-label={`Aanmelden bij ${appName}`}
-                            className="bg-[#0070f3] text-white rounded px-[12px] py-[12px] text-[14px] font-semibold no-underline"
+                            className="bg-[#0070f3] text-white rounded px-3 py-3 text-[14px] font-semibold no-underline"
                         >
                             Aanmelden bij {appName}
                         </Button>
                     </Section>
-                    <Text className="text-black text-[14px] leading-[24px]">
+                    <Text className="text-black text-[14px] leading-6">
                         Werkt de knop niet? Kopieer en plak dan de onderstaande
                         link in uw browser:
                     </Text>
                     <Link
                         href={url}
-                        className="text-[#0070f3] text-[12px] leading-[24px] break-all block mb-[16px]"
+                        className="text-[#0070f3] text-[12px] leading-6 break-all block mb-4"
                     >
                         {url}
                     </Link>
-                    <Text className="text-[#666666] text-[12px] leading-[24px] mt-[5px] block text-center mb-[32px]">
+                    <Text className="text-[#666666] text-[12px] leading-6 mt-1.25 block text-center mb-8">
                         Deze link is éénmalig en voor 15 minuten geldig.
                     </Text>
-                    <Text className="text-black text-[14px] leading-[24px]">
+                    <Text className="text-black text-[14px] leading-6">
                         Indien u dit niet heeft aangevraagd, kunt u deze e-mail
                         negeren.
                     </Text>
-                    <Text className="text-black text-[14px] leading-[24px] mt-[32px]">
+                    <Text className="text-black text-[14px] leading-6 mt-8">
                         Met vriendelijke groet,
                         <br />
                         {senderName ? senderName : `Het ${appName} team`}
                     </Text>
                     <Link
                         href={appBaseUrl}
-                        className="text-[#666666] text-[12px] leading-[24px] mt-[20px] block text-center"
+                        className="text-[#666666] text-[12px] leading-6 mt-5 block text-center"
                     >
                         {appName}
                     </Link>
-                    <Text className="text-[#666666] text-[12px] leading-[24px] mt-[5px] block text-center">
+                    <Text className="text-[#666666] text-[12px] leading-6 mt-1.25 block text-center">
                         {`Deze link is aangemaakt op ${emailTimestamp}`}
                     </Text>
                 </Container>
