061671e06ed3c4370f6c5d96c257be8e16a86433
Mert Bora İnevi <bora.inevi@nmi-agro.nl>
2026-01-21 16:13:32 +0100

    Make the new field common redirect route separate

--- fdm-app/app/routes/farm.$b_id_farm.$calendar.field.new.fields.$b_id._index.tsx
+++ fdm-app/app/routes/farm.$b_id_farm.$calendar.field.new.fields.$b_id._index.tsx
@@ -21,7 +21,6 @@ import {
     type LoaderFunctionArgs,
     type MetaFunction,
     NavLink,
-    redirect,
     useLoaderData,
     useLocation,
 } from "react-router"
@@ -116,24 +115,12 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
             })
         }
 
-        const calendar = getCalendar(params)
-        const url = new URL(request.url)
-
-        // Obtain the fieldIds
-        // Redirect to one of them if not viewing a field currently.
         const b_id = params.b_id
-        let fieldIds: string[] = b_id ? [b_id] : []
-        const fieldIdsParam = url.searchParams.get("fieldIds")
-        if (fieldIdsParam && fieldIdsParam.length > 0)
-            fieldIds = fieldIdsParam.split(",")
         if (!b_id) {
-            if (fieldIds.length === 0) {
-                // Redirect to the fields table if no field can be shown at all.
-                return redirect(`/farm/${b_id_farm}/${calendar}/field`)
-            }
-            return redirect(
-                `/farm/${b_id_farm}/${calendar}/field/new/fields/${fieldIds[0]}${url.search}`,
-            )
+            throw data("Field ID is required", {
+                status: 400,
+                statusText: "Field ID is required",
+            })
         }
 
         // Get the session
